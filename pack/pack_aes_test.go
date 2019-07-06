b063573cf3d489cd988d35dd433817409f3e7793
Alopex6414 <1029535012@qq.com>
2019-07-06 21:24:17 +0800

    Add test function.

--- pack/pack_aes_test.go
+++ pack/pack_aes_test.go
@@ -25,3 +25,28 @@ func TestAESEncrypt(t *testing.T) {
 		t.Fatal("Error AES Encrypt:", err)
 	}
 }
+
+func BenchmarkPackAESOne(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "test/file.txt"
+		r, err := PackAESOne(src)
+		if err != nil {
+			b.Fatal("Error Pack AES One:", err)
+		}
+		err = ioutil.WriteFile("test/file_aes.txt", r, 0644)
+		if err != nil {
+			b.Fatal("Error Write AES One:", err)
+		}
+	}
+}
+
+func BenchmarkAESEncrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte("hello,world!")
+		key := []byte("Satellite-266414")
+		_, err := AESEncrypt(src, key)
+		if err != nil {
+			b.Fatal("Error AES Encrypt:", err)
+		}
+	}
+}
