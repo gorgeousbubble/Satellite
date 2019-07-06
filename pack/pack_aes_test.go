b8e56f6885549b98548ab48d15abf90af0775867
Alopex6414 <1029535012@qq.com>
2019-07-06 22:48:23 +0800

    Add Benchmark test code.

--- pack/pack_aes_test.go
+++ pack/pack_aes_test.go
@@ -52,6 +52,35 @@ func TestAESEncrypt(t *testing.T) {
 	}
 }
 
+func BenchmarkPackAES(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []string{"test/file_1.txt", "test/file_2.txt", "test/file_3.txt", "test/file_4.txt", "test/file_5.txt"}
+		dest := "test/file_aes.txt"
+		err := PackAES(src, dest)
+		if err != nil {
+			b.Fatal("Error Pack AES:", err)
+		}
+	}
+}
+
+func BenchmarkPackAESOneGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		var r []byte
+		src := "test/file.txt"
+		wg.Add(1)
+		err := PackAESOneGo(src, &r, &wg)
+		if err != nil {
+			b.Fatal("Error Pack AES One Go:", err)
+		}
+		wg.Wait()
+		err = ioutil.WriteFile("test/file_aes.txt", r, 0644)
+		if err != nil {
+			b.Fatal("Error Write AES One Go:", err)
+		}
+	}
+}
+
 func BenchmarkPackAESOne(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := "test/file.txt"
