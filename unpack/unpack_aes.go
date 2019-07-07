b9063e85f568ec8ae0762d841a2f8372ac5b145d
Alopex6414 <1029535012@qq.com>
2019-07-07 11:16:58 +0800

    Add AES Decrypt code.

--- unpack/unpack_aes.go
+++ unpack/unpack_aes.go
@@ -1,1 +1,35 @@
 package unpack
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"log"
+)
+
+func AESDecrypt(src, key []byte) (dest []byte, err error) {
+	dest = []byte{}
+	err = nil
+	// key length should be 16, 24, 32
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Println("Error key length:", err)
+		return dest, err
+	}
+	// calculate block size
+	blockSize := block.BlockSize()
+	// encrypt mode
+	blockMode := cipher.NewCBCDecrypter(block,key[:blockSize])
+	// create slice
+	dest = make([]byte, len(src))
+	// decrypt
+	blockMode.CryptBlocks(dest, src)
+	// delete block data
+	dest = PKCS7UnPadding(dest)
+	return dest, err
+}
+
+func PKCS7UnPadding(src []byte) []byte {
+	size := len(src)
+	unpadding := int(src[size-1])
+	return src[:(size - unpadding)]
+}
