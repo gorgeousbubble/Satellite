9ad4860346e54fc5f2ae5195a8f482a5513df05e
Alopex6414 <1029535012@qq.com>
2019-07-17 22:46:18 +0800

    Add test code.

--- utils/rsa_test.go
+++ utils/rsa_test.go
@@ -17,3 +17,23 @@ func TestGenRSAKey2Memory(t *testing.T) {
 		t.Errorf("Error Generate RSA Key to Memory.")
 	}
 }
+
+func BenchmarkGenRSAKey2File(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		err := GenRSAKey2File(1024)
+		if err != nil {
+			b.Errorf("Error Generate RSA Key to File.")
+		}
+	}
+}
+
+func BenchmarkGenRSAKey2Memory(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var private []byte
+		var public []byte
+		err := GenRSAKey2Memory(&private, &public, 1024)
+		if err != nil {
+			b.Errorf("Error Generate RSA Key to Memory.")
+		}
+	}
+}
