0b800925ee55c8eebb23ab948bbc67b576ce7faf
Alopex6414 <1029535012@qq.com>
2019-07-17 23:21:12 +0800

    Add RSA code.

--- unpack/unpack_rsa.go
+++ unpack/unpack_rsa.go
@@ -1,2 +1,27 @@
 package unpack
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	. "satellite/utils"
+)
+
+func RSADecrypt(src []byte) (dest []byte, err error) {
+	block, _ := pem.Decode(RSAPrivateKey)
+	if block == nil {
+		err = errors.New("RSA Private Key Error")
+		return dest, err
+	}
+	pri, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return dest, err
+	}
+	dest, err = rsa.DecryptPKCS1v15(rand.Reader, pri, src)
+	if err != nil {
+		return dest, err
+	}
+	return dest, err
+}
