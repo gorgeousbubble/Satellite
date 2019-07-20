e380d5c8fb072fd51562f00b024849b1323a3adf
Alopex6414 <1029535012@qq.com>
2019-07-20 23:13:57 +0800

    Add base64 code.

--- unpack/unpack_base64.go
+++ unpack/unpack_base64.go
@@ -1,6 +1,14 @@
 package unpack
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"sync"
+)
+
+func Base64DecryptGo(str string, r *string, wg *sync.WaitGroup) {
+	*r = Base64Decrypt(str)
+	wg.Done()
+}
 
 func Base64Decrypt(str string) string {
 	s, _ := base64.StdEncoding.DecodeString(str)
--- unpack/unpack_base64_test.go
+++ unpack/unpack_base64_test.go
@@ -2,8 +2,26 @@ package unpack
 
 import (
 	"io/ioutil"
+	"sync"
 	"testing"
 )
+
+func TestBase64DecryptGo(t *testing.T) {
+	var wg sync.WaitGroup
+	var r string
+	src := "aGVsbG8sd29ybGQh"
+	dest := "hello,world!"
+	wg.Add(1)
+	go Base64DecryptGo(src, &r, &wg)
+	wg.Wait()
+	if r != dest {
+		t.Errorf("Error Decrypt Base64.")
+	}
+	err := ioutil.WriteFile("../test/data/unpack/file.txt", []byte(r), 0644)
+	if err != nil {
+		t.Fatal("Error Write Base64 One:", err)
+	}
+}
 
 func TestBase64Decrypt(t *testing.T) {
 	src := "aGVsbG8sd29ybGQh"
@@ -15,6 +33,25 @@ func TestBase64Decrypt(t *testing.T) {
 	err := ioutil.WriteFile("../test/data/unpack/file.txt", []byte(r), 0644)
 	if err != nil {
 		t.Fatal("Error Write Base64 One:", err)
+	}
+}
+
+func BenchmarkBase64DecryptGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		var r string
+		src := "aGVsbG8sd29ybGQh"
+		dest := "hello,world!"
+		wg.Add(1)
+		go Base64DecryptGo(src, &r, &wg)
+		wg.Wait()
+		if r != dest {
+			b.Errorf("Error Decrypt Base64.")
+		}
+		err := ioutil.WriteFile("../test/data/unpack/file.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write Base64 One:", err)
+		}
 	}
 }
 
