0500cabd20a57a6f95bf44b4197f9e2d25010537
Alopex6414 <1029535012@qq.com>
2019-07-20 15:50:15 +0800

    Add base64 test code.

--- pack/pack_base64_test.go
+++ pack/pack_base64_test.go
@@ -13,3 +13,14 @@ func TestBase64Encrypt(t *testing.T) {
 		t.Fatal("Error Write Base64 One:", err)
 	}
 }
+
+func BenchmarkBase64Encrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		r := Base64Encrypt(src)
+		err := ioutil.WriteFile("test/file_base64.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write Base64 One:", err)
+		}
+	}
+}
