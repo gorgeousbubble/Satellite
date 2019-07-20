3a808c8de21ce969c4f57b010bb76815c97ee10d
Alopex6414 <1029535012@qq.com>
2019-07-20 16:12:41 +0800

    Add base64 test code

--- unpack/unpack_base64_test.go
+++ unpack/unpack_base64_test.go
@@ -17,3 +17,18 @@ func TestBase64Decrypt(t *testing.T) {
 		t.Fatal("Error Write Base64 One:", err)
 	}
 }
+
+func BenchmarkBase64Decrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "aGVsbG8sd29ybGQh"
+		dest := "hello,world!"
+		r := Base64Decrypt(src)
+		if r != dest {
+			b.Errorf("Error Decrypt Base64.")
+		}
+		err := ioutil.WriteFile("test/file.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write Base64 One:", err)
+		}
+	}
+}
