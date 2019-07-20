408b1a53337518199b0299bac480bcd1b06f2962
Alopex6414 <1029535012@qq.com>
2019-07-20 17:30:41 +0800

    Add test code.

--- pack/pack_base64.go
+++ pack/pack_base64.go
@@ -1,6 +1,30 @@
 package pack
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"sync"
+)
+
+/*func PackBase64One(srcfile string) (r string, err error) {
+	// first, open the file
+	file, err := os.Open(srcfile)
+	if err != nil {
+		log.Println("Error open file:", err)
+		return r, err
+	}
+	defer file.Close()
+	// second, read file data
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Println("Error read file:", err)
+		return r, err
+	}
+}*/
+
+func Base64EncrypteGo(str string, r *string, wg *sync.WaitGroup) {
+	*r = Base64Encrypt(str)
+	wg.Done()
+}
 
 func Base64Encrypt(str string) string {
 	s := []byte(str)
--- pack/pack_base64_test.go
+++ pack/pack_base64_test.go
@@ -2,8 +2,26 @@ package pack
 
 import (
 	"io/ioutil"
+	"sync"
 	"testing"
 )
+
+func TestBase64EncrypteGo(t *testing.T) {
+	var wg sync.WaitGroup
+	var r string
+	src := "hello,world!"
+	dest := "aGVsbG8sd29ybGQh"
+	wg.Add(1)
+	go Base64EncrypteGo(src, &r, &wg)
+	wg.Wait()
+	if r != dest {
+		t.Fatal("Error Encrypt Base64.")
+	}
+	err := ioutil.WriteFile("../test/data/pack/file_base64.txt", []byte(r), 0644)
+	if err != nil {
+		t.Fatal("Error Write Base64 One:", err)
+	}
+}
 
 func TestBase64Encrypt(t *testing.T) {
 	src := "hello,world!"
