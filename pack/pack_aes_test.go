0435fe859cd1ee789c2dad633dc9ebf00f5756d8
Alopex6414 <1029535012@qq.com>
2019-07-20 16:52:37 +0800

    Change test code.

--- pack/pack_aes_test.go
+++ pack/pack_aes_test.go
@@ -19,26 +19,26 @@ func TestPackAES(t *testing.T) {
 func TestPackAESOneGo(t *testing.T) {
 	var wg sync.WaitGroup
 	var r []byte
-	src := "test/file.txt"
+	src := "../test/data/pack/file.txt"
 	wg.Add(1)
 	err := PackAESOneGo(src, &r, &wg)
 	if err != nil {
 		t.Fatal("Error Pack AES One Go:", err)
 	}
 	wg.Wait()
-	err = ioutil.WriteFile("test/file_aes.txt", r, 0644)
+	err = ioutil.WriteFile("../test/data/pack/file_aes.txt", r, 0644)
 	if err != nil {
 		t.Fatal("Error Write AES One Go:", err)
 	}
 }
 
 func TestPackAESOne(t *testing.T) {
-	src := "test/file.txt"
+	src := "../test/data/pack/file.txt"
 	r, err := PackAESOne(src)
 	if err != nil {
 		t.Fatal("Error Pack AES One:", err)
 	}
-	err = ioutil.WriteFile("test/file_aes.txt", r, 0644)
+	err = ioutil.WriteFile("../test/data/pack/file_aes.txt", r, 0644)
 	if err != nil {
 		t.Fatal("Error Write AES One:", err)
 	}
@@ -52,7 +52,7 @@ func TestAESEncryptGo(t *testing.T) {
 	wg.Add(1)
 	go AESEncryptGo(src, key, &r, &wg)
 	wg.Wait()
-	err := ioutil.WriteFile("test/file_aes.txt", r, 0644)
+	err := ioutil.WriteFile("../test/data/pack/file_aes.txt", r, 0644)
 	if err != nil {
 		t.Fatal("Error Write AES Encrypt:", err)
 	}
@@ -65,7 +65,7 @@ func TestAESEncrypt(t *testing.T) {
 	if err != nil {
 		t.Fatal("Error AES Encrypt:", err)
 	}
-	err = ioutil.WriteFile("test/file_aes.txt", r, 0644)
+	err = ioutil.WriteFile("../test/data/pack/file_aes.txt", r, 0644)
 	if err != nil {
 		t.Fatal("Error Write AES Encrypt:", err)
 	}
@@ -82,8 +82,8 @@ func TestSplitByte(t *testing.T) {
 
 func BenchmarkPackAES(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		src := []string{"test/file_1.txt", "test/file_2.txt", "test/file_3.txt", "test/file_4.txt", "test/file_5.txt"}
-		dest := "test/file_aes.txt"
+		src := []string{"../test/data/pack/file_1.txt", "../test/data/pack/file_2.txt", "../test/data/pack/file_3.txt", "../test/data/pack/file_4.txt", "../test/data/pack/file_5.txt"}
+		dest := "../test/data/pack/file_aes.txt"
 		err := PackAES(src, dest)
 		if err != nil {
 			b.Fatal("Error Pack AES:", err)
@@ -95,14 +95,14 @@ func BenchmarkPackAESOneGo(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		var wg sync.WaitGroup
 		var r []byte
-		src := "test/file.txt"
+		src := "../test/data/pack/file.txt"
 		wg.Add(1)
 		err := PackAESOneGo(src, &r, &wg)
 		if err != nil {
 			b.Fatal("Error Pack AES One Go:", err)
 		}
 		wg.Wait()
-		err = ioutil.WriteFile("test/file_aes.txt", r, 0644)
+		err = ioutil.WriteFile("../test/data/pack/file_aes.txt", r, 0644)
 		if err != nil {
 			b.Fatal("Error Write AES One Go:", err)
 		}
@@ -111,12 +111,12 @@ func BenchmarkPackAESOneGo(b *testing.B) {
 
 func BenchmarkPackAESOne(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		src := "test/file.txt"
+		src := "../test/data/pack/file.txt"
 		r, err := PackAESOne(src)
 		if err != nil {
 			b.Fatal("Error Pack AES One:", err)
 		}
-		err = ioutil.WriteFile("test/file_aes.txt", r, 0644)
+		err = ioutil.WriteFile("../test/data/pack/file_aes.txt", r, 0644)
 		if err != nil {
 			b.Fatal("Error Write AES One:", err)
 		}
@@ -132,7 +132,7 @@ func BenchmarkAESEncryptGo(b *testing.B) {
 		wg.Add(1)
 		go AESEncryptGo(src, key, &r, &wg)
 		wg.Wait()
-		err := ioutil.WriteFile("test/file_aes.txt", r, 0644)
+		err := ioutil.WriteFile("../test/data/pack/file_aes.txt", r, 0644)
 		if err != nil {
 			b.Fatal("Error Write AES Encrypt:", err)
 		}
