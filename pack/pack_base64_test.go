479c5fa49de3003c835ee2a2e283d4fbdf876b41
Alopex6414 <1029535012@qq.com>
2019-07-20 17:03:08 +0800

    Change test code.

--- pack/pack_base64_test.go
+++ pack/pack_base64_test.go
@@ -7,8 +7,12 @@ import (
 
 func TestBase64Encrypt(t *testing.T) {
 	src := "hello,world!"
+	dest := "aGVsbG8sd29ybGQh"
 	r := Base64Encrypt(src)
-	err := ioutil.WriteFile("../test/data/file_base64.txt", []byte(r), 0644)
+	if r != dest {
+		t.Fatal("Error Encrypt Base64.")
+	}
+	err := ioutil.WriteFile("../test/data/pack/file_base64.txt", []byte(r), 0644)
 	if err != nil {
 		t.Fatal("Error Write Base64 One:", err)
 	}
@@ -17,8 +21,12 @@ func TestBase64Encrypt(t *testing.T) {
 func BenchmarkBase64Encrypt(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := "hello,world!"
+		dest := "aGVsbG8sd29ybGQh"
 		r := Base64Encrypt(src)
-		err := ioutil.WriteFile("test/file_base64.txt", []byte(r), 0644)
+		if r != dest {
+			b.Fatal("Error Encrypt Base64.")
+		}
+		err := ioutil.WriteFile("../test/data/pack/file_base64.txt", []byte(r), 0644)
 		if err != nil {
 			b.Fatal("Error Write Base64 One:", err)
 		}
