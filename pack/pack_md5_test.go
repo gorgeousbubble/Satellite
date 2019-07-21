c903fe0a27c94976f10b37d97428ab0123b9df85
Alopex6414 <1029535012@qq.com>
2019-07-21 17:12:56 +0800

    Add MD5 test code.

---
+++ pack/pack_md5_test.go
@@ -0,0 +1,55 @@
+package pack
+
+import (
+	"io/ioutil"
+	"sync"
+	"testing"
+)
+
+func TestMD5EncryptGo(t *testing.T) {
+	var wg sync.WaitGroup
+	var r [16]byte
+	src := []byte("hello,world!")
+	wg.Add(1)
+	go MD5EncryptGo(src, &r, &wg)
+	wg.Wait()
+	err := ioutil.WriteFile("../test/data/pack/file_md5.txt", r[:15], 0644)
+	if err != nil {
+		t.Fatal("Error Write MD5 Encrypt:", err)
+	}
+}
+
+func TestMD5Encrypt(t *testing.T) {
+	src := []byte("hello,world!")
+	r := MD5Encrypt(src)
+	err := ioutil.WriteFile("../test/data/pack/file_md5.txt", r[:15], 0644)
+	if err != nil {
+		t.Fatal("Error Write MD5 Encrypt:", err)
+	}
+}
+
+func BenchmarkMD5EncryptGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		var r [16]byte
+		src := []byte("hello,world!")
+		wg.Add(1)
+		go MD5EncryptGo(src, &r, &wg)
+		wg.Wait()
+		err := ioutil.WriteFile("../test/data/pack/file_md5.txt", r[:15], 0644)
+		if err != nil {
+			b.Fatal("Error Write MD5 Encrypt:", err)
+		}
+	}
+}
+
+func BenchmarkMD5Encrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte("hello,world!")
+		r := MD5Encrypt(src)
+		err := ioutil.WriteFile("../test/data/pack/file_md5.txt", r[:15], 0644)
+		if err != nil {
+			b.Fatal("Error Write MD5 Encrypt:", err)
+		}
+	}
+}
