929d68bc3f55e8115b8446092121b4b4d06af3e1
Alopex6414 <1029535012@qq.com>
2019-07-21 09:31:48 +0800

    Add base64 code.

--- unpack/unpack_base64.go
+++ unpack/unpack_base64.go
@@ -1,9 +1,53 @@
 package unpack
 
 import (
+	"bytes"
 	"encoding/base64"
+	"io/ioutil"
+	"log"
+	. "satellite/utils"
+	"strings"
 	"sync"
 )
+
+func UnpackBase64One(data []byte, head TUnpackBase64One, destpath string) (err error) {
+	// initial, fill the name
+	var s []byte
+	for _, v := range head.Name {
+		if v == byte(0) {
+			break
+		}
+		s = append(s, v)
+	}
+	destfile := destpath + string(s)
+	// first, split the data slice
+	ss, err := SplitByte(data, ConstBase64BufferSize)
+	if err != nil {
+		log.Println("Error split bytes:", err)
+		return err
+	}
+	size := len(data) % ConstBase64BufferSize
+	if size != 0 {
+		last := len(data) / ConstBase64BufferSize
+		ss[last] = append(ss[last][:0], ss[last][:size]...)
+	}
+	// second, we can call Base64Decrypt function
+	wg := &sync.WaitGroup{}
+	rr := make([]string, len(ss))
+	for k, v := range ss {
+		wg.Add(1)
+		go Base64DecryptGo(string(v), &rr[k], wg)
+	}
+	wg.Wait()
+	dest := strings.Join(rr, "")
+	// fourth, create the origin file
+	err = ioutil.WriteFile(destfile, []byte(dest), 0644)
+	if err != nil {
+		log.Println("Error write to destfile:", err)
+		return err
+	}
+	return err
+}
 
 func Base64DecryptGo(str string, r *string, wg *sync.WaitGroup) {
 	*r = Base64Decrypt(str)
--- unpack/unpack_com.go
+++ unpack/unpack_com.go
@@ -1,6 +1,6 @@
 package unpack
 
-// pack aes
+// unpack aes
 type TUnpackAES struct {
 	Name		[]byte		// [32]byte/256bit
 	Author		[]byte		// [16]byte/128bit
@@ -12,4 +12,16 @@ type TUnpackAESOne struct {
 	Key			[]byte		// [16]byte/128bit
 	OriginSize	[]byte		// [4]byte/32bit
 	CryptSize	[]byte		// [4]byte/32bit
+}
+
+// unpack base64
+type TUnpackBase64 struct {
+	Name		[]byte		// [32]byte/256bit
+	Author		[]byte		// [16]byte/128bit
+	Number		[]byte		// [4]byte/32bit
+}
+
+type TUnpackBase64One struct {
+	Name		[]byte		// [32]byte/256bit
+	Size		[]byte		// [4]byte/32bit
 }
