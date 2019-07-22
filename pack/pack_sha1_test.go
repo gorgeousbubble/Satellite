407b8e8fea91bb39004715242c05089debe2ba4b
Alopex6414 <1029535012@qq.com>
2019-07-22 22:14:48 +0800

    Add sha1 test code.

--- pack/pack_sha1_test.go
+++ pack/pack_sha1_test.go
@@ -1,16 +1,46 @@
 package pack
 
 import (
+	"crypto/sha1"
 	"io/ioutil"
+	"sync"
 	"testing"
 )
+
+func TestSHA1EncryptGo(t *testing.T) {
+	var wg sync.WaitGroup
+	var r [sha1.Size]byte
+	src := []byte("hello,world!")
+	wg.Add(1)
+	go SHA1EncryptGo(src, &r, &wg)
+	wg.Wait()
+	err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:(sha1.Size-1)], 0644)
+	if err != nil {
+		t.Fatal("Error Write SHA1 Encrypt:", err)
+	}
+}
 
 func TestSHA1Encrypt(t *testing.T) {
 	src := []byte("hello,world!")
 	r := SHA1Encrypt(src)
-	err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:19], 0644)
+	err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:(sha1.Size-1)], 0644)
 	if err != nil {
 		t.Fatal("Error Write SHA1 Encrypt:", err)
+	}
+}
+
+func BenchmarkSHA1EncryptGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		var r [sha1.Size]byte
+		src := []byte("hello,world!")
+		wg.Add(1)
+		go SHA1EncryptGo(src, &r, &wg)
+		wg.Wait()
+		err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:(sha1.Size-1)], 0644)
+		if err != nil {
+			b.Fatal("Error Write SHA1 Encrypt:", err)
+		}
 	}
 }
 
@@ -18,7 +48,7 @@ func BenchmarkSHA1Encrypt(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := []byte("hello,world!")
 		r := SHA1Encrypt(src)
-		err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:19], 0644)
+		err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:(sha1.Size-1)], 0644)
 		if err != nil {
 			b.Fatal("Error Write SHA1 Encrypt:", err)
 		}
