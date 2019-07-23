4d049dd770c7766b3a0c1e0e3020748c0df40747
Alopex6414 <1029535012@qq.com>
2019-07-23 21:32:49 +0800

    Add sha256 code.

--- pack/pack_sha256.go
+++ pack/pack_sha256.go
@@ -1,6 +1,14 @@
 package pack
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"sync"
+)
+
+func SHA256EncryptGo(data []byte, r *[sha256.Size]byte, wg *sync.WaitGroup) {
+	*r = sha256.Sum256(data)
+	wg.Done()
+}
 
 func SHA256Encrypt(data []byte) [sha256.Size]byte {
 	return sha256.Sum256(data)
