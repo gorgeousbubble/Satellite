0d5fcd0170b37cb085a63fb6152c5af9e243392e
Alopex6414 <1029535012@qq.com>
2019-07-24 21:54:52 +0800

    Add test code.

--- pack/pack_md5_test.go
+++ pack/pack_md5_test.go
@@ -7,6 +7,33 @@ import (
 	"testing"
 )
 
+func TestMD5Check(t *testing.T) {
+	src := "hello,world!"
+	dest := "c0e84e870874dd37ed0d164c7986f03a"
+	b := MD5Check(src, dest)
+	if !b {
+		t.Fatal("Error Check MD5:")
+	}
+}
+
+func TestMD5Check2(t *testing.T) {
+	src := "Nice to meet you~"
+	dest := "c0e84e870874dd37ed0d164c7986f03a"
+	b := MD5Check(src, dest)
+	if b {
+		t.Fatal("Error Check MD5:")
+	}
+}
+
+func TestMD5Encode(t *testing.T) {
+	src := "hello,world!"
+	r := MD5Encode(src)
+	err := ioutil.WriteFile("../test/data/pack/file_md5.txt", []byte(r), 0644)
+	if err != nil {
+		t.Fatal("Error Write MD5 Encode:", err)
+	}
+}
+
 func TestMD5EncryptGo(t *testing.T) {
 	var wg sync.WaitGroup
 	var r [md5.Size]byte
@@ -26,6 +53,39 @@ func TestMD5Encrypt(t *testing.T) {
 	err := ioutil.WriteFile("../test/data/pack/file_md5.txt", r[:md5.Size], 0644)
 	if err != nil {
 		t.Fatal("Error Write MD5 Encrypt:", err)
+	}
+}
+
+func BenchmarkMD5Check(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		dest := "c0e84e870874dd37ed0d164c7986f03a"
+		f := MD5Check(src, dest)
+		if !f {
+			b.Fatal("Error Check MD5:")
+		}
+	}
+}
+
+func BenchmarkMD5Check2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "Nice to meet you~"
+		dest := "c0e84e870874dd37ed0d164c7986f03a"
+		f := MD5Check(src, dest)
+		if f {
+			b.Fatal("Error Check MD5:")
+		}
+	}
+}
+
+func BenchmarkMD5Encode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		r := MD5Encode(src)
+		err := ioutil.WriteFile("../test/data/pack/file_md5.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write MD5 Encode:", err)
+		}
 	}
 }
 
