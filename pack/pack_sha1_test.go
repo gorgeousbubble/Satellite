b08e3469ad1c77bab5c85fdf78b2ffb3f3511d90
Alopex6414 <1029535012@qq.com>
2019-07-24 22:15:29 +0800

    Add sha1 test code.

--- pack/pack_sha1_test.go
+++ pack/pack_sha1_test.go
@@ -7,6 +7,33 @@ import (
 	"testing"
 )
 
+func TestSHA1Check(t *testing.T) {
+	src := "hello,world!"
+	dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+	r := SHA1Check(src, dest)
+	if !r {
+		t.Fatal("Error Check SHA1:")
+	}
+}
+
+func TestSHA1Check2(t *testing.T) {
+	src := "Nice to meet you~"
+	dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+	r := SHA1Check(src, dest)
+	if r {
+		t.Fatal("Error Check SHA1:")
+	}
+}
+
+func TestSHA1Encode(t *testing.T) {
+	src := "hello,world!"
+	r := SHA1Encode(src)
+	err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", []byte(r), 0644)
+	if err != nil {
+		t.Fatal("Error Write SHA1 Encode:", err)
+	}
+}
+
 func TestSHA1EncryptGo(t *testing.T) {
 	var wg sync.WaitGroup
 	var r [sha1.Size]byte
@@ -26,6 +53,39 @@ func TestSHA1Encrypt(t *testing.T) {
 	err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", r[:sha1.Size], 0644)
 	if err != nil {
 		t.Fatal("Error Write SHA1 Encrypt:", err)
+	}
+}
+
+func BenchmarkSHA1Check(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+		r := SHA1Check(src, dest)
+		if !r {
+			b.Fatal("Error Check SHA1:")
+		}
+	}
+}
+
+func BenchmarkSHA1Check2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "Nice to meet you~"
+		dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+		r := SHA1Check(src, dest)
+		if r {
+			b.Fatal("Error Check SHA1:")
+		}
+	}
+}
+
+func BenchmarkSHA1Encode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		r := SHA1Encode(src)
+		err := ioutil.WriteFile("../test/data/pack/file_sha1.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write SHA1 Encode:", err)
+		}
 	}
 }
 
