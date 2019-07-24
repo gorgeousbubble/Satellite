cbad6e5b8e8c480c6c054d3ae86f9fab331a0ff7
Alopex6414 <1029535012@qq.com>
2019-07-24 22:08:27 +0800

    Add sha512 code.

--- pack/pack_sha512.go
+++ pack/pack_sha512.go
@@ -2,8 +2,23 @@ package pack
 
 import (
 	"crypto/sha512"
+	"encoding/hex"
 	"sync"
 )
+
+func SHA512Check(src string, dest string) bool {
+	b := false
+	if SHA512Encode(src) == dest {
+		b = true
+	}
+	return b
+}
+
+func SHA512Encode(src string) string {
+	h := sha512.New()
+	h.Write([]byte(src))
+	return hex.EncodeToString(h.Sum(nil))
+}
 
 func SHA512EncryptGo(data []byte, r *[sha512.Size]byte, wg *sync.WaitGroup) {
 	*r = sha512.Sum512(data)
