96ba116da3153adf8108d5f8880677d8ed73ef16
Alopex6414 <1029535012@qq.com>
2019-07-24 22:38:32 +0800

    Add hmac code.

--- pack/pack_hmac.go
+++ pack/pack_hmac.go
@@ -1,2 +1,27 @@
 package pack
 
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+)
+
+func HMAC_SHA1(src, key string) string {
+	m := hmac.New(sha1.New, []byte(key))
+	m.Write([]byte(src))
+	return hex.EncodeToString(m.Sum(nil))
+}
+
+func HMAC_SHA256(src, key string) string {
+	m := hmac.New(sha256.New, []byte(key))
+	m.Write([]byte(src))
+	return hex.EncodeToString(m.Sum(nil))
+}
+
+func HMAC_SHA512(src, key string) string {
+	m := hmac.New(sha512.New, []byte(key))
+	m.Write([]byte(src))
+	return hex.EncodeToString(m.Sum(nil))
+}
