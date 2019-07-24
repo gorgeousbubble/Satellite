1505cd138d94c123b5131b543a58ca4085c9dab0
Alopex6414 <1029535012@qq.com>
2019-07-24 22:01:59 +0800

    Add sha1 code.

--- pack/pack_sha1.go
+++ pack/pack_sha1.go
@@ -2,8 +2,23 @@ package pack
 
 import (
 	"crypto/sha1"
+	"encoding/hex"
 	"sync"
 )
+
+func SHA1Check(src string, dest string) bool {
+	b := false
+	if SHA1Encode(src) == dest {
+		b = true
+	}
+	return b
+}
+
+func SHA1Encode(src string) string {
+	h := sha1.New()
+	h.Write([]byte(src))
+	return hex.EncodeToString(h.Sum(nil))
+}
 
 func SHA1EncryptGo(data []byte, r *[sha1.Size]byte, wg *sync.WaitGroup)  {
 	*r = sha1.Sum(data)
