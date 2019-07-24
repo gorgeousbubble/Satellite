98daaff30cd7d8a627fc7daaf94a0a1377cfcc21
Alopex6414 <1029535012@qq.com>
2019-07-24 21:43:53 +0800

    Add md5 encode & check func.

--- pack/pack_md5.go
+++ pack/pack_md5.go
@@ -2,8 +2,23 @@ package pack
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"sync"
 )
+
+func MD5Check(src string, dest string) bool {
+	b := false
+	if MD5Encode(src) == dest {
+		b = true
+	}
+	return b
+}
+
+func MD5Encode(src string) string {
+	h := md5.New()
+	h.Write([]byte(src))
+	return hex.EncodeToString(h.Sum(nil))
+}
 
 func MD5EncryptGo(data []byte, r *[md5.Size]byte, wg *sync.WaitGroup) {
 	*r = md5.Sum(data)
