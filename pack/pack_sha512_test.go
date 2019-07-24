65af6bf6f0bf7d233f74318fc3dd2440926b033f
Alopex6414 <1029535012@qq.com>
2019-07-24 22:32:00 +0800

    Add sha512 test code.

--- pack/pack_sha512_test.go
+++ pack/pack_sha512_test.go
@@ -7,6 +7,33 @@ import (
 	"testing"
 )
 
+func TestSHA512Check(t *testing.T) {
+	src := "hello,world!"
+	dest := "fa9edcfdaab7a4165f8d2593f04077d46aca957493e7e181ca479582d519a299d967305294d5d46fb5e0158240441b94cd96510c2311bdc86870e5ebf3efe60c"
+	r := SHA512Check(src, dest)
+	if !r {
+		t.Fatal("Error Check SHA512:")
+	}
+}
+
+func TestSHA512Check2(t *testing.T) {
+	src := "Nice to meet you~"
+	dest := "fa9edcfdaab7a4165f8d2593f04077d46aca957493e7e181ca479582d519a299d967305294d5d46fb5e0158240441b94cd96510c2311bdc86870e5ebf3efe60c"
+	r := SHA512Check(src, dest)
+	if r {
+		t.Fatal("Error Check SHA512:")
+	}
+}
+
+func TestSHA512Encode(t *testing.T) {
+	src := "hello,world!"
+	r := SHA512Encode(src)
+	err := ioutil.WriteFile("../test/data/pack/file_sha512.txt", []byte(r), 0644)
+	if err != nil {
+		t.Fatal("Error Write SHA512 Encode:", err)
+	}
+}
+
 func TestSHA512EncryptGo(t *testing.T) {
 	var wg sync.WaitGroup
 	var r [sha512.Size]byte
@@ -26,6 +53,39 @@ func TestSHA512Encrypt(t *testing.T) {
 	err := ioutil.WriteFile("../test/data/pack/file_sha512.txt", r[:sha512.Size], 0644)
 	if err != nil {
 		t.Fatal("Error Write SHA512 Encrypt:", err)
+	}
+}
+
+func BenchmarkSHA512Check(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		dest := "fa9edcfdaab7a4165f8d2593f04077d46aca957493e7e181ca479582d519a299d967305294d5d46fb5e0158240441b94cd96510c2311bdc86870e5ebf3efe60c"
+		r := SHA512Check(src, dest)
+		if !r {
+			b.Fatal("Error Check SHA512:")
+		}
+	}
+}
+
+func BenchmarkSHA512Check2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "Nice to meet you~"
+		dest := "fa9edcfdaab7a4165f8d2593f04077d46aca957493e7e181ca479582d519a299d967305294d5d46fb5e0158240441b94cd96510c2311bdc86870e5ebf3efe60c"
+		r := SHA512Check(src, dest)
+		if r {
+			b.Fatal("Error Check SHA512:")
+		}
+	}
+}
+
+func BenchmarkSHA512Encode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		r := SHA512Encode(src)
+		err := ioutil.WriteFile("../test/data/pack/file_sha512.txt", []byte(r), 0644)
+		if err != nil {
+			b.Fatal("Error Write SHA512 Encode:", err)
+		}
 	}
 }
 
