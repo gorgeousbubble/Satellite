b146a96e3fd3ea91b46d916f024faf2db912a325
Alopex6414 <1029535012@qq.com>
2019-07-24 21:24:12 +0800

    Add sha512 test code.

--- pack/pack_sha512_test.go
+++ pack/pack_sha512_test.go
@@ -1,2 +1,27 @@
 package pack
 
+import (
+	"crypto/sha512"
+	"io/ioutil"
+	"testing"
+)
+
+func TestSHA512Encrypt(t *testing.T) {
+	src := []byte("hello,world!")
+	r := SHA512Encrypt(src)
+	err := ioutil.WriteFile("../test/data/pack/file_sha512.txt", r[:sha512.Size], 0644)
+	if err != nil {
+		t.Fatal("Error Write SHA512 Encrypt:", err)
+	}
+}
+
+func BenchmarkSHA512Encrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte("hello,world!")
+		r := SHA512Encrypt(src)
+		err := ioutil.WriteFile("../test/data/pack/file_sha512.txt", r[:sha512.Size], 0644)
+		if err != nil {
+			b.Fatal("Error Write SHA512 Encrypt:", err)
+		}
+	}
+}
