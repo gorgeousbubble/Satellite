88992f8cb9e5837882331f6240e4ba67be39f013
Alopex6414 <1029535012@qq.com>
2019-07-25 21:23:13 +0800

    Add test code.

--- pack/pack_hmac_test.go
+++ pack/pack_hmac_test.go
@@ -1,2 +1,27 @@
 package pack
 
+import (
+	"testing"
+)
+
+func TestHMAC_SHA1(t *testing.T) {
+	src := "hello,world!"
+	key := "Alopex6414"
+	dest := "e6a6dd9149277e8a5a189e2041a4e5ef50b5bca0"
+	r := HMAC_SHA1(src, key)
+	if r != dest {
+		t.Fatal("Error HMAC SHA1:")
+	}
+}
+
+func BenchmarkHMAC_SHA1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		key := "Alopex6414"
+		dest := "e6a6dd9149277e8a5a189e2041a4e5ef50b5bca0"
+		r := HMAC_SHA1(src, key)
+		if r != dest {
+			b.Fatal("Error HMAC SHA1:")
+		}
+	}
+}
