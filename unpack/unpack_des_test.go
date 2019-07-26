1633db79fbfc1096c1968fdb87832bbbe869b2f0
Alopex6414 <1029535012@qq.com>
2019-07-26 22:20:49 +0800

    Add DES test code.

--- unpack/unpack_des_test.go
+++ unpack/unpack_des_test.go
@@ -2,8 +2,23 @@ package unpack
 
 import (
 	"io/ioutil"
+	"sync"
 	"testing"
 )
+
+func TestDESDecryptGo(t *testing.T) {
+	var dest []byte
+	var wg sync.WaitGroup
+	src := []byte{0x8F, 0x34, 0x1C, 0x84, 0xBB, 0xB4, 0x58, 0xA8, 0x54, 0x8B, 0xC0, 0x9E, 0x2F, 0x55, 0x01, 0xF2}
+	key := []byte("hyacinth")
+	wg.Add(1)
+	go DESDecryptGo(src, key, &dest, &wg)
+	wg.Wait()
+	err := ioutil.WriteFile("../test/data/unpack/file.txt", dest, 0644)
+	if err != nil {
+		t.Fatal("Error Write DES One:", err)
+	}
+}
 
 func TestDESDecrypt(t *testing.T) {
 	src := []byte{0x8F, 0x34, 0x1C, 0x84, 0xBB, 0xB4, 0x58, 0xA8, 0x54, 0x8B, 0xC0, 0x9E, 0x2F, 0x55, 0x01, 0xF2}
@@ -15,6 +30,22 @@ func TestDESDecrypt(t *testing.T) {
 	err = ioutil.WriteFile("../test/data/unpack/file.txt", r, 0644)
 	if err != nil {
 		t.Fatal("Error Write DES One:", err)
+	}
+}
+
+func BenchmarkDESDecryptGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var dest []byte
+		var wg sync.WaitGroup
+		src := []byte{0x8F, 0x34, 0x1C, 0x84, 0xBB, 0xB4, 0x58, 0xA8, 0x54, 0x8B, 0xC0, 0x9E, 0x2F, 0x55, 0x01, 0xF2}
+		key := []byte("hyacinth")
+		wg.Add(1)
+		go DESDecryptGo(src, key, &dest, &wg)
+		wg.Wait()
+		err := ioutil.WriteFile("../test/data/unpack/file.txt", dest, 0644)
+		if err != nil {
+			b.Fatal("Error Write DES One:", err)
+		}
 	}
 }
 
