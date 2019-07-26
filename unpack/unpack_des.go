de2900656a5f5117fffe9c5219187c9aeeda7f20
Alopex6414 <1029535012@qq.com>
2019-07-26 21:20:26 +0800

    Add DES code.

--- unpack/unpack_des.go
+++ unpack/unpack_des.go
@@ -1,3 +1,35 @@
 package unpack
 
+import (
+	"crypto/cipher"
+	"crypto/des"
+	"log"
+)
 
+func DESDecrypt(src, key []byte) (dest []byte, err error) {
+	// key length should be 8
+	block, err := des.NewCipher(key)
+	if err != nil {
+		log.Println("Error key length:", err)
+		return dest, err
+	}
+	// calculate block size
+	blockSize := block.BlockSize()
+	// encrypt mode
+	blockMode := cipher.NewCBCDecrypter(block,key[:blockSize])
+	// create slice
+	dest = make([]byte, len(src))
+	// decrypt
+	blockMode.CryptBlocks(dest, src)
+	// delete block data
+	dest = PKCS7UnPadding(dest)
+	return dest, err
+}
+
+func PKCS5UnPadding(src []byte) []byte {
+	// don't need do anything
+	/*size := len(src)
+	unpadding := int(src[size-1])
+	return src[:(size - unpadding)]*/
+	return src
+}
