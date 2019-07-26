50833c238f9aed542832438c165885cb86073ec3
Alopex6414 <1029535012@qq.com>
2019-07-26 21:06:44 +0800

    Add DES code.

--- pack/pack_des.go
+++ pack/pack_des.go
@@ -1,3 +1,37 @@
 package pack
 
+import (
+	"bytes"
+	"crypto/cipher"
+	"crypto/des"
+	"log"
+)
 
+func DESEncrypt(src, key []byte) (dest []byte, err error) {
+	// key length should be 8
+	block, err := des.NewCipher(key)
+	if err != nil {
+		log.Println("Error key length:", err)
+		return dest, err
+	}
+	// calculate block size
+	blockSize := block.BlockSize()
+	// fill block data
+	src = PKCS5Padding(src, blockSize)
+	// encrypt mode
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	// create slice
+	dest = make([]byte, len(src))
+	// encrypt
+	blockMode.CryptBlocks(dest, src)
+	return dest, err
+}
+
+func PKCS5Padding(src []byte, size int) []byte {
+	var padding int
+	if len(src)%size != 0 {
+		padding = size - len(src) % size
+	}
+	text := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, text...)
+}
--- pack/pack_des_test.go
+++ pack/pack_des_test.go
@@ -1,3 +1,34 @@
 package pack
 
+import (
+	"io/ioutil"
+	"testing"
+)
 
+func TestDESEncrypt(t *testing.T) {
+	src := []byte("hello,world!")
+	key := []byte("hyacinth")
+	r, err := DESEncrypt(src, key)
+	if err != nil {
+		t.Fatal("Error DES Encrypt:", err)
+	}
+	err = ioutil.WriteFile("../test/data/pack/file_des.txt", r, 0644)
+	if err != nil {
+		t.Fatal("Error Write DES Encrypt:", err)
+	}
+}
+
+func BenchmarkDESEncrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte("hello,world!")
+		key := []byte("hyacinth")
+		r, err := DESEncrypt(src, key)
+		if err != nil {
+			b.Fatal("Error DES Encrypt:", err)
+		}
+		err = ioutil.WriteFile("../test/data/pack/file_des.txt", r, 0644)
+		if err != nil {
+			b.Fatal("Error Write DES Encrypt:", err)
+		}
+	}
+}
