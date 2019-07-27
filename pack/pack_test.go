bd769a1565d3d88b7b2a6d4e18736267f7ab032b
Alopex6414 <1029535012@qq.com>
2019-07-27 22:26:39 +0800

    Add test code.

--- pack/pack_test.go
+++ pack/pack_test.go
@@ -51,3 +51,63 @@ func TestPack5(t *testing.T) {
 		t.Fatal("Error Pack:", err)
 	}
 }
+
+func BenchmarkPack(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []string{"../test/data/pack/file_1.txt", "../test/data/pack/file_2.txt", "../test/data/pack/file_3.txt", "../test/data/pack/file_4.txt", "../test/data/pack/file_5.txt"}
+		dest := "../test/data/pack/file_aes.txt"
+		algorithm := "AES"
+		err := Pack(src, dest, algorithm)
+		if err != nil {
+			b.Fatal("Error Pack:", err)
+		}
+	}
+}
+
+func BenchmarkPack2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []string{"../test/data/pack/file_1.txt", "../test/data/pack/file_2.txt", "../test/data/pack/file_3.txt", "../test/data/pack/file_4.txt", "../test/data/pack/file_5.txt"}
+		dest := "../test/data/pack/file_aes.txt"
+		algorithm := "DES"
+		err := Pack(src, dest, algorithm)
+		if err != nil {
+			b.Fatal("Error Pack:", err)
+		}
+	}
+}
+
+func BenchmarkPack3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []string{"../test/data/pack/file_1.txt", "../test/data/pack/file_2.txt", "../test/data/pack/file_3.txt", "../test/data/pack/file_4.txt", "../test/data/pack/file_5.txt"}
+		dest := "../test/data/pack/file_aes.txt"
+		algorithm := "3DES"
+		err := Pack(src, dest, algorithm)
+		if err != nil {
+			b.Fatal("Error Pack:", err)
+		}
+	}
+}
+
+func BenchmarkPack4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []string{"../test/data/pack/file_1.txt", "../test/data/pack/file_2.txt", "../test/data/pack/file_3.txt", "../test/data/pack/file_4.txt", "../test/data/pack/file_5.txt"}
+		dest := "../test/data/pack/file_aes.txt"
+		algorithm := "RSA"
+		err := Pack(src, dest, algorithm)
+		if err != nil {
+			b.Fatal("Error Pack:", err)
+		}
+	}
+}
+
+func BenchmarkPack5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []string{"../test/data/pack/file_1.txt", "../test/data/pack/file_2.txt", "../test/data/pack/file_3.txt", "../test/data/pack/file_4.txt", "../test/data/pack/file_5.txt"}
+		dest := "../test/data/pack/file_aes.txt"
+		algorithm := "BASE64"
+		err := Pack(src, dest, algorithm)
+		if err != nil {
+			b.Fatal("Error Pack:", err)
+		}
+	}
+}
--- unpack/unpack_test.go
+++ unpack/unpack_test.go
@@ -46,3 +46,58 @@ func TestUnpack5(t *testing.T) {
 		t.Fatal("Error Unpack:", err)
 	}
 }
+
+func BenchmarkUnpack(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		srcfile := "../test/data/unpack/file_aes.txt"
+		destpath := "../test/data/unpack/"
+		err := Unpack(srcfile, destpath)
+		if err != nil {
+			b.Fatal("Error Unpack:", err)
+		}
+	}
+}
+
+func BenchmarkUnpack2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		srcfile := "../test/data/unpack/file_des.txt"
+		destpath := "../test/data/unpack/"
+		err := Unpack(srcfile, destpath)
+		if err != nil {
+			b.Fatal("Error Unpack:", err)
+		}
+	}
+}
+
+func BenchmarkUnpack3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		srcfile := "../test/data/unpack/file_3des.txt"
+		destpath := "../test/data/unpack/"
+		err := Unpack(srcfile, destpath)
+		if err != nil {
+			b.Fatal("Error Unpack:", err)
+		}
+	}
+}
+
+func BenchmarkUnpack4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		srcfile := "../test/data/unpack/file_rsa.txt"
+		destpath := "../test/data/unpack/"
+		err := Unpack(srcfile, destpath)
+		if err != nil {
+			b.Fatal("Error Unpack:", err)
+		}
+	}
+}
+
+func BenchmarkUnpack5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		srcfile := "../test/data/unpack/file_base64.txt"
+		destpath := "../test/data/unpack/"
+		err := Unpack(srcfile, destpath)
+		if err != nil {
+			b.Fatal("Error Unpack:", err)
+		}
+	}
+}
