27ec0c193b677bc3d0e539d047e72a58a2ba26d0
Alopex6414 <1029535012@qq.com>
2019-07-28 21:47:02 +0800

    Add select sort.

--- sorts/bubble_sort.go
+++ sorts/bubble_sort.go
@@ -2,14 +2,14 @@ package sorts
 
 func BubbleSort(s []int) {
 	for i := 0; i < len(s)-1; i++ {
-		ex := false
+		k := false
 		for j := 0; j < len(s)-i-1; j++ {
 			if s[j] > s[j+1] {
 				s[j], s[j+1] = s[j+1], s[j]
-				ex = true
+				k = true
 			}
 		}
-		if !ex {
+		if !k {
 			return
 		}
 	}
---
+++ sorts/select_sort.go
@@ -0,0 +1,15 @@
+package sorts
+
+func SelectSort(s []int) {
+	for i := 0; i < len(s)-1; i++ {
+		k := i
+		for j := i + 1; j < len(s); j++ {
+			if s[i] > s[j] {
+				k = j
+			}
+		}
+		if k != i {
+			s[i], s[k] = s[k], s[i]
+		}
+	}
+}
