29edae1133d843064c99fce7dc63dd86cb387226
Alopex6414 <1029535012@qq.com>
2019-07-29 21:06:15 +0800

    Add shell sort test code.

--- sorts/shell_sort_test.go
+++ sorts/shell_sort_test.go
@@ -1,3 +1,21 @@
 package sorts
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestShellSort(t *testing.T) {
+	s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+	fmt.Println("Shell sort:")
+	fmt.Println("Before sort slice:", s)
+	SelectSort(s)
+	fmt.Println("After sort slice:", s)
+}
+
+func BenchmarkShellSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+		ShellSort(s)
+	}
+}
