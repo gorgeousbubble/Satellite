876ff68132143f7b988efdd838a9d44806d3c909
Alopex6414 <1029535012@qq.com>
2019-07-29 22:13:41 +0800

    Add heap sort code.

--- sorts/heap_sort.go
+++ sorts/heap_sort.go
@@ -1,3 +1,30 @@
 package sorts
 
+func shift(s []int, l int, h int) {
+	i := l
+	j := 2*i + 1
+	k := s[i]
+	for j <= h {
+		if j < h && s[j] < s[j+1] {
+			j++
+		}
+		if k < s[j] {
+			s[i]  = s[j]
+			i = j
+			j = 2*i + 1
+		} else {
+			break
+		}
+	}
+	s[i] = k
+}
 
+func HeapSort(s []int) {
+	for i := len(s)/2 - 1; i >= 0; i-- {
+		shift(s, i, len(s)-1)
+	}
+	for j := len(s) - 1; j > 0; j-- {
+		s[0], s[j] = s[j], s[0]
+		shift(s, 0, j-1)
+	}
+}
--- sorts/heap_sort_test.go
+++ sorts/heap_sort_test.go
@@ -1,3 +1,21 @@
 package sorts
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestHeapSort(t *testing.T) {
+	s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+	fmt.Println("Heap sort:")
+	fmt.Println("Before sort slice:", s)
+	HeapSort(s)
+	fmt.Println("After sort slice:", s)
+}
+
+func BenchmarkHeapSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+		HeapSort(s)
+	}
+}
