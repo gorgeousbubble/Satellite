0b3dbb912d1c5cece4cdce313184a374b5b2992f
Alopex6414 <1029535012@qq.com>
2019-07-30 23:06:40 +0800

    Add merge sort code.

--- sorts/merge_sort_test.go
+++ sorts/merge_sort_test.go
@@ -1,3 +1,21 @@
 package sorts
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestMergeSort(t *testing.T) {
+	s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+	fmt.Println("Merge sort:")
+	fmt.Println("Before sort slice:", s)
+	MergeSort(s, 0, 9)
+	fmt.Println("After sort slice:", s)
+}
+
+func BenchmarkMergeSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+		MergeSort(s, 0, 9)
+	}
+}
