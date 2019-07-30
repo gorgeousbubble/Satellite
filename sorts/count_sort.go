015c3688c86fc3ed2b4f2b315cb6fcb7d9cca0d0
Alopex6414 <1029535012@qq.com>
2019-07-30 23:23:53 +0800

    Add count sort code.

--- sorts/count_sort.go
+++ sorts/count_sort.go
@@ -1,3 +1,21 @@
 package sorts
 
-
+func CountSort(s []int) {
+	max := s[0]
+	for i := 1; i < len(s); i++ {
+		if max < s[i] {
+			max = s[i]
+		}
+	}
+	arr := make([]int, max+1)
+	for j := 0; j < len(s); j++ {
+		arr[s[j]]++
+	}
+	k := 0
+	for m, n := range arr {
+		for p := 0; p < n; p++ {
+			s[k] = m
+			k++
+		}
+	}
+}
