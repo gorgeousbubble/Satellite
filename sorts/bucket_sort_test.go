ee8792d3f94919f7f241dd978edceb2bc6d61167
Alopex6414 <1029535012@qq.com>
2019-07-31 00:13:22 +0800

    Add test code.

--- sorts/bucket_sort_test.go
+++ sorts/bucket_sort_test.go
@@ -1,3 +1,21 @@
 package sorts
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestBucketSort(t *testing.T) {
+	s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+	fmt.Println("Bucket sort:")
+	fmt.Println("Before sort slice:", s)
+	BucketSort(s, 1)
+	fmt.Println("After sort slice:", s)
+}
+
+func BenchmarkBucketSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+		BucketSort(s, 1)
+	}
+}
