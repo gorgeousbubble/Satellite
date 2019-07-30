ac3ccfb2cc8410a2d8f537322f421a70b6f2a377
Alopex6414 <1029535012@qq.com>
2019-07-30 23:25:53 +0800

    Change count sort test code.

--- sorts/count_sort_test.go
+++ sorts/count_sort_test.go
@@ -1,3 +1,21 @@
 package sorts
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestCountSort(t *testing.T) {
+	s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+	fmt.Println("Count sort:")
+	fmt.Println("Before sort slice:", s)
+	CountSort(s)
+	fmt.Println("After sort slice:", s)
+}
+
+func BenchmarkCountSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := []int{1, 3, 8, 2, 4, 9, 7, 5, 6, 0}
+		CountSort(s)
+	}
+}
