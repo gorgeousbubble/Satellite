ee0aba92875ee9b12e3848bcef716970d54257d7
Alopex6414 <1029535012@qq.com>
2019-07-31 22:03:08 +0800

    Add compress zip test code.

--- comp/comp_zip_test.go
+++ comp/comp_zip_test.go
@@ -1,5 +1,23 @@
-package main
+package comp
 
-func main() {
-	$END$
+import "testing"
+
+func TestCompressZipOne(t *testing.T) {
+	src := "../test/data/comp/file.txt"
+	dest := "../test/data/comp/file.zip"
+	err := CompressZipOne(src, dest)
+	if err != nil {
+		t.Fatal("Error Compress Zip One:", err)
+	}
+}
+
+func BenchmarkCompressZipOne(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/comp/file.txt"
+		dest := "../test/data/comp/file.zip"
+		err := CompressZipOne(src, dest)
+		if err != nil {
+			b.Fatal("Error Compress Zip One:", err)
+		}
+	}
 }
