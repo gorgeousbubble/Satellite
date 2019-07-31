54b9aede795f9e69d19b84fe29bb6d97210c2544
Alopex6414 <1029535012@qq.com>
2019-07-31 21:40:06 +0800

    Add comp zip code.

---
+++ comp/comp_zip.go
@@ -0,0 +1,54 @@
+package comp
+
+import (
+	"archive/zip"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func CompressZipOne(src string, dest string) (err error) {
+	// create the dest zip file...
+	file, err := os.Create(dest)
+	if err != nil {
+		log.Println("Error create file:", err)
+		return err
+	}
+	defer file.Close()
+	// apply one zip writer to write file
+	archive := zip.NewWriter(file)
+	defer archive.Close()
+	// compress zip files...
+	err = filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Println("Error compress file:", err)
+			return err
+		}
+		// get file information header
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			log.Println("Error get file info header:", err)
+			return err
+		}
+		// assign compress method
+		header.Method = zip.Deflate
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			log.Println("Error create compress file header:", err)
+			return err
+		}
+		// write compress data into file
+		_, err = io.Copy(writer, file)
+		if err != nil {
+			log.Println("Error write compress data into file:", err)
+			return err
+		}
+		return err
+	})
+	if err != nil {
+		log.Println("Error compress file:", err)
+		return err
+	}
+	return err
+}
