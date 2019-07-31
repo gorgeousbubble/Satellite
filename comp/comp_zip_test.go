39420d491011aec07bb918d412091703156009da
Alopex6414 <1029535012@qq.com>
2019-07-31 22:10:37 +0800

    Add compress zip test code.

--- comp/comp_zip_test.go
+++ comp/comp_zip_test.go
@@ -1,6 +1,18 @@
 package comp
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
+
+func TestCompressZipOneGo(t *testing.T) {
+	var wg sync.WaitGroup
+	src := "../test/data/comp/file.txt"
+	dest := "../test/data/comp/file.zip"
+	wg.Add(1)
+	go CompressZipOneGo(src, dest, &wg)
+	wg.Wait()
+}
 
 func TestCompressZipOne(t *testing.T) {
 	src := "../test/data/comp/file.txt"
@@ -8,6 +20,17 @@ func TestCompressZipOne(t *testing.T) {
 	err := CompressZipOne(src, dest)
 	if err != nil {
 		t.Fatal("Error Compress Zip One:", err)
+	}
+}
+
+func BenchmarkCompressZipOneGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		src := "../test/data/comp/file.txt"
+		dest := "../test/data/comp/file.zip"
+		wg.Add(1)
+		go CompressZipOneGo(src, dest, &wg)
+		wg.Wait()
 	}
 }
 
