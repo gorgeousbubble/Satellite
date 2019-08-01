8848eb91327a0aa01eff31091680d2fc18e307b1
Alopex6414 <1029535012@qq.com>
2019-08-01 22:00:12 +0800

    Add decompress zip test code.

--- decomp/decomp_zip_test.go
+++ decomp/decomp_zip_test.go
@@ -1,3 +1,23 @@
 package decomp
 
+import "testing"
 
+func TestDeCompressZip(t *testing.T) {
+	src := "../test/data/decomp/file.zip"
+	dest := "../test/data/decomp/"
+	err := DeCompressZip(src, dest)
+	if err != nil {
+		t.Fatal("Error DeCompress Zip:", err)
+	}
+}
+
+func BenchmarkDeCompressZip(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/decomp/file.zip"
+		dest := "../test/data/decomp/"
+		err := DeCompressZip(src, dest)
+		if err != nil {
+			b.Fatal("Error DeCompress Zip:", err)
+		}
+	}
+}
