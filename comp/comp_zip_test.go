39a92542e5ae1e183cdb9f3ab9d02100b533600a
Alopex6414 <1029535012@qq.com>
2019-08-01 22:30:56 +0800

    Delete compress zip one code.

--- comp/comp_zip_test.go
+++ comp/comp_zip_test.go
@@ -13,15 +13,6 @@ func TestCompressZip(t *testing.T) {
 	}
 }
 
-func TestCompressZipOne(t *testing.T) {
-	src := "../test/data/comp/file.txt"
-	dest := "../test/data/comp/file.zip"
-	err := CompressZipOne(src, dest)
-	if err != nil {
-		t.Fatal("Error Compress Zip One:", err)
-	}
-}
-
 func BenchmarkCompressZip(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := []string{"../test/data/comp/file_1.txt", "../test/data/comp/file_2.txt", "../test/data/comp/file_3.txt", "../test/data/comp/file_4.txt", "../test/data/comp/file_5.txt"}
@@ -29,17 +20,6 @@ func BenchmarkCompressZip(b *testing.B) {
 		err := CompressZip(src, dest)
 		if err != nil {
 			b.Fatal("Error Compress Zip:", err)
-		}
-	}
-}
-
-func BenchmarkCompressZipOne(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		src := "../test/data/comp/file.txt"
-		dest := "../test/data/comp/file.zip"
-		err := CompressZipOne(src, dest)
-		if err != nil {
-			b.Fatal("Error Compress Zip One:", err)
 		}
 	}
 }
