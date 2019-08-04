543398fb7c81a5d32ecba40db615b7cf90a7f4c1
Alopex6414 <1029535012@qq.com>
2019-08-04 09:58:10 +0800

    Add decompress tar test code.

--- decomp/decomp_tar_test.go
+++ decomp/decomp_tar_test.go
@@ -1,3 +1,23 @@
 package decomp
 
+import "testing"
 
+func TestDeCompressTarGz(t *testing.T) {
+	src := "../test/data/decomp/file.tar.gz"
+	dest := "../test/data/decomp/"
+	err := DeCompressTarGz(src, dest)
+	if err != nil {
+		t.Fatal("Error DeCompress Tar Gz:", err)
+	}
+}
+
+func BenchmarkDeCompressTarGz(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/decomp/file.tar.gz"
+		dest := "../test/data/decomp/"
+		err := DeCompressTarGz(src, dest)
+		if err != nil {
+			b.Fatal("Error DeCompress Tar Gz:", err)
+		}
+	}
+}
