ab57239484c410c25b535b5441c6e30b0dae3442
Alopex6414 <1029535012@qq.com>
2019-08-04 09:56:51 +0800

    Add decompress tar code.

--- decomp/decomp_tar.go
+++ decomp/decomp_tar.go
@@ -1,3 +1,55 @@
 package decomp
 
+import (
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
+func DeCompressTarGz(src string, dest string) (err error) {
+	// open the src tar ball file...
+	file, err := os.Open(src)
+	if err != nil {
+		log.Println("Error open tar gz file:", err)
+		return err
+	}
+	defer file.Close()
+	gr, err := gzip.NewReader(file)
+	if err != nil {
+		log.Println("Error new gzip reader:", err)
+		return err
+	}
+	defer gr.Close()
+	tr := tar.NewReader(gr)
+	// loop decompress src list files
+	for {
+		header, err := tr.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return err
+			}
+		}
+		name := dest + header.Name
+		err = os.MkdirAll(string([]rune(name)[0:strings.LastIndex(name, "/")]), 0755)
+		if err != nil {
+			log.Println("Error make dir all:", err)
+			return err
+		}
+		f, err := os.Create(name)
+		if err != nil {
+			log.Println("Error create name:", err)
+			return err
+		}
+		_, err = io.Copy(f, tr)
+		if err != nil {
+			log.Println("Error write decompress date:", err)
+			return err
+		}
+	}
+	return err
+}
