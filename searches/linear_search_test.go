40e242528b5f1fad87e82084f715577af4b1e9b6
Alopex6414 <1029535012@qq.com>
2019-08-05 21:53:40 +0800

    Add linear search test code.

--- searches/linear_search_test.go
+++ searches/linear_search_test.go
@@ -1,3 +1,25 @@
 package searches
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestLinearSearch(t *testing.T) {
+	s := []int{0, 2, 4, 6, 8, 10, 12, 14, 16, 18}
+	fmt.Println("Linear Search:")
+	index := LinearSearch(s, 10)
+	if index == -1 {
+		fmt.Println("Number not found")
+	} else {
+		fmt.Println("Index: ", index)
+		fmt.Println("s[", index, "] = ", s[index])
+	}
+}
+
+func BenchmarkLinearSearch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := []int{0, 2, 4, 6, 8, 10, 12, 14, 16, 18}
+		LinearSearch(s, 10)
+	}
+}
