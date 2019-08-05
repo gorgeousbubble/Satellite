0942076a647d64fbc24b7792ecd6ee322740d804
Alopex6414 <1029535012@qq.com>
2019-08-05 21:28:49 +0800

    Add insert searches code.

--- searches/insert_search.go
+++ searches/insert_search.go
@@ -1,3 +1,19 @@
 package searches
 
-
+func InsertSearch(s []int, k int) int {
+	l  := 0
+	h := len(s) - 1
+	t := 0
+	for l < h {
+		t += 1
+		m := l + int((h - l) * (k - s[l])/(s[h] - s[l]))
+		if k < s[m] {
+			h = m - 1
+		}else if k > s[m] {
+			l = m + 1
+		}else {
+			return m
+		}
+	}
+	return -1
+}
