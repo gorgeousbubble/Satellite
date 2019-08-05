406cedc9b8ea6d80bb6a6d20cd2a3fe7f2a7c011
Alopex6414 <1029535012@qq.com>
2019-08-05 21:32:03 +0800

    Add insert search test code.

--- searches/insert_search_test.go
+++ searches/insert_search_test.go
@@ -1,3 +1,25 @@
 package searches
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestInsertSearch(t *testing.T) {
+	s := []int{0, 2, 4, 6, 8, 10, 12, 14, 16, 18}
+	fmt.Println("Insert Search:")
+	index := InsertSearch(s, 10)
+	if index == -1 {
+		fmt.Println("Number not found")
+	} else {
+		fmt.Println("Index: ", index)
+		fmt.Println("s[", index, "] = ", s[index])
+	}
+}
+
+func BenchmarkInsertSearch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := []int{0, 2, 4, 6, 8, 10, 12, 14, 16, 18}
+		InsertSearch(s, 10)
+	}
+}
