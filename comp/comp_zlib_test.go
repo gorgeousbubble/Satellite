1cb99cc6a87d8083ba6128f78fe6123bd2281162
Alopex6414 <1029535012@qq.com>
2019-08-06 21:18:56 +0800

    Add compress zlib test code.

--- comp/comp_zlib_test.go
+++ comp/comp_zlib_test.go
@@ -1,3 +1,26 @@
 package comp
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestCompressZlib(t *testing.T) {
+	src := "hello,world!"
+	fmt.Println("Before Compress Zlib:", src)
+	dest, err := CompressZlib([]byte(src))
+	if err != nil {
+		t.Fatal("Error Compress Zlib:", err)
+	}
+	fmt.Println("After Compress Zlib:", dest)
+}
+
+func BenchmarkCompressZlib(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		_, err := CompressZlib([]byte(src))
+		if err != nil {
+			b.Fatal("Error Compress Zlib:", err)
+		}
+	}
+}
