61e12e10e044bb328546ee3ab5fedffd85d5931d
Alopex6414 <1029535012@qq.com>
2019-08-06 21:08:50 +0800

    Add compress zlib code.

--- comp/comp_zlib.go
+++ comp/comp_zlib.go
@@ -1,3 +1,24 @@
 package comp
 
+import (
+	"bytes"
+	"compress/zlib"
+	"log"
+)
 
+func CompressZlib(src []byte) (dest []byte, err error) {
+	var in bytes.Buffer
+	w := zlib.NewWriter(&in)
+	_, err = w.Write(src)
+	if err != nil {
+		log.Println("Error compress zlib:", err)
+		return dest, err
+	}
+	err = w.Close()
+	if err != nil {
+		log.Println("Error close zlib writer:", err)
+		return dest, err
+	}
+	dest = in.Bytes()
+	return dest, err
+}
