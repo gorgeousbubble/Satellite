a81176c58040240674c46851a0e0d81f284c1509
Alopex6414 <1029535012@qq.com>
2019-08-06 21:37:01 +0800

    Add decompress zlib code.

--- decomp/decomp_zlib.go
+++ decomp/decomp_zlib.go
@@ -1,3 +1,25 @@
 package decomp
 
+import (
+	"bytes"
+	"compress/zlib"
+	"io"
+	"log"
+)
 
+func DeCompressZlib(src []byte) (dest []byte, err error) {
+	var out bytes.Buffer
+	b := bytes.NewReader(src)
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		log.Println("Error decompress zlib:", err)
+		return dest, err
+	}
+	_, err = io.Copy(&out, r)
+	if err != nil {
+		log.Println("Error writer zlib:", err)
+		return dest, err
+	}
+	dest = out.Bytes()
+	return dest, err
+}
