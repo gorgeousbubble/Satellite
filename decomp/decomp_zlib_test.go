5017396721f24b12c0e3de6b6afcd0f0de06224a
Alopex6414 <1029535012@qq.com>
2019-08-06 21:44:23 +0800

    Add decompress zlib test code.

--- decomp/decomp_zlib_test.go
+++ decomp/decomp_zlib_test.go
@@ -1,3 +1,26 @@
 package decomp
 
+import (
+	"fmt"
+	"testing"
+)
 
+func TestDeCompressZlib(t *testing.T) {
+	src := []byte{120, 156, 202, 72, 205, 201, 201, 215, 41, 207, 47, 202, 73, 81, 4, 4, 0, 0, 255, 255, 30, 221, 4, 138}
+	fmt.Println("Before DeCompress Zlib:", string(src))
+	dest, err := DeCompressZlib(src)
+	if err != nil {
+		t.Fatal("Error DeCompress Zlib:", err)
+	}
+	fmt.Println("After DeCompress Zlib:", string(dest))
+}
+
+func BenchmarkDeCompressZlib(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte{120, 156, 202, 72, 205, 201, 201, 215, 41, 207, 47, 202, 73, 81, 4, 4, 0, 0, 255, 255, 30, 221, 4, 138}
+		_, err := DeCompressZlib(src)
+		if err != nil {
+			b.Fatal("Error DeCompress Zlib:", err)
+		}
+	}
+}
