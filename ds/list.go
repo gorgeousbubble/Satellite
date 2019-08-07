9b7acd36da058946004ff09d2c386fd8805436b7
Alopex6414 <1029535012@qq.com>
2019-08-07 21:10:34 +0800

    Add list code.

--- ds/list.go
+++ ds/list.go
@@ -1,3 +1,22 @@
 package ds
 
+type Object interface {}
+
+type Node struct {
+	data Object
+	next *Node
+}
+
+type List struct {
+	head *Node
+	tail *Node
+	size uint64
+}
+
+func (list *List) Init() {
+	list.head = nil
+	list.tail = nil
+	list.size = 0
+}
+
 
