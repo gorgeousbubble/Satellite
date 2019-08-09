ddecac4cb296f770c14b722efbb86767f8bd4a60
Alopex6414 <1029535012@qq.com>
2019-08-09 23:31:59 +0800

    Add cmd common code

--- cmd/cmd_com.go
+++ cmd/cmd_com.go
@@ -1,3 +1,31 @@
 package cmd
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
+var Help bool
+
+func init() {
+	flag.BoolVar(&Help, "h", false, "show help")
+
+	flag.Usage = usage
+}
+
+func usage() {
+	_, err := fmt.Fprintf(os.Stderr, `Satellite Project - We share beautiful dreams together.
+Version: v1.00a
+Author: alopex
+
+Usage: satellite [-h]
+Options:
+`)
+	if err != nil {
+		log.Println("Error print information:", err)
+		os.Exit(1)
+	}
+	flag.PrintDefaults()
+}
