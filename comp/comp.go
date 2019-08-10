4d58a15fe089f68965eb7a9e24c31198d3d2c597
Alopex6414 <1029535012@qq.com>
2019-08-10 14:17:57 +0800

    Add compress code.

--- comp/comp.go
+++ comp/comp.go
@@ -1,5 +1,19 @@
-package main
+package comp
 
-func main() {
-	$END$
+import (
+	"github.com/pkg/errors"
+	"log"
+)
+
+func Compress(src []string, dest string, algorithm string) (err error) {
+	switch algorithm {
+	case "tar":
+		err = CompressTarGz(src, dest)
+	case "zip":
+		err = CompressZip(src, dest)
+	default:
+		err = errors.New("Undefined compress algorithm.")
+		log.Println(err)
+	}
+	return err
 }
