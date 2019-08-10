5a27a16f5297ae8548ded6324528bf09118c999e
Alopex6414 <1029535012@qq.com>
2019-08-10 14:39:37 +0800

    Add decompress test code.

---
+++ decomp/decomp_test.go
@@ -0,0 +1,43 @@
+package decomp
+
+import "testing"
+
+func TestDeCompress(t *testing.T) {
+	src := "../test/data/decomp/file.tar.gz"
+	dest := "../test/data/decomp/"
+	err := DeCompress(src, dest, "tar")
+	if err != nil {
+		t.Fatal("Error DeCompress:", err)
+	}
+}
+
+func TestDeCompress2(t *testing.T) {
+	src := "../test/data/decomp/file.zip"
+	dest := "../test/data/decomp/"
+	err := DeCompress(src, dest, "zip")
+	if err != nil {
+		t.Fatal("Error DeCompress:", err)
+	}
+}
+
+func BenchmarkDeCompress(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/decomp/file.tar.gz"
+		dest := "../test/data/decomp/"
+		err := DeCompress(src, dest, "tar")
+		if err != nil {
+			b.Fatal("Error DeCompress:", err)
+		}
+	}
+}
+
+func BenchmarkDeCompress2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/decomp/file.zip"
+		dest := "../test/data/decomp/"
+		err := DeCompress(src, dest, "zip")
+		if err != nil {
+			b.Fatal("Error DeCompress:", err)
+		}
+	}
+}
