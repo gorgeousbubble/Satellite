b876325f07abe61415fea33240220febd1efbdd8
Alopex6414 <1029535012@qq.com>
2019-08-11 23:00:29 +0800

    Chang logging code.(still remain bugs)

--- logging/logging.go
+++ logging/logging.go
@@ -2,9 +2,11 @@ package logging
 
 import (
 	"fmt"
+	"log"
 	"os"
 	. "satellite/global"
 	. "satellite/utils"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -31,12 +33,19 @@ func init() {
 		}
 	}
 	// Only keep 5 log files
-	files, err := ListFiles(path)
+	files, err := ListFiles(path, AppName)
 	if err != nil {
 		fmt.Println("Error open log file:", err)
 		os.Exit(1)
 	}
-	fmt.Println(files)
+	if len(files) >= LogNumber {
+		sort.Sort(FileInfos(files))
+		err = os.Remove(files[0].Name())
+		if err != nil {
+			fmt.Println("Error remove log file:", err)
+			os.Exit(1)
+		}
+	}
 	// Create logging file
 	file, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
@@ -44,5 +53,6 @@ func init() {
 		os.Exit(1)
 	}
 	defer file.Close()
-
+	// Set log settings
+	log.SetOutput(file)
 }
