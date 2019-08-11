004eef979c4980a4e264edd26bf06e7557a78756
Alopex6414 <1029535012@qq.com>
2019-08-11 23:01:25 +0800

    Update files code, add function.

--- utils/files.go
+++ utils/files.go
@@ -4,7 +4,22 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+type FileInfos []os.FileInfo
+
+func (s FileInfos) Len() int {
+	return len(s)
+}
+
+func (s FileInfos) Less(i, j int) bool {
+	return s[i].ModTime().Before(s[j].ModTime())
+}
+
+func (s FileInfos) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
 
 func PathExist(dir string) (is bool, err error) {
 	_, err = os.Stat(dir)
@@ -19,7 +34,7 @@ func PathExist(dir string) (is bool, err error) {
 	return true, err
 }
 
-func ListFiles(dir string) (files []os.FileInfo, err error) {
+func ListFiles(dir string, term string) (files []os.FileInfo, err error) {
 	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -27,7 +42,9 @@ func ListFiles(dir string) (files []os.FileInfo, err error) {
 		if info.IsDir() {
 			return err
 		}
-		files = append(files, info)
+		if strings.Contains(info.Name(), term) {
+			files = append(files, info)
+		}
 		return err
 	})
 	if err != nil {
