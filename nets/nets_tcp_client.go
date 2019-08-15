65e6ed06229effc62a0d3450a6c33367b4bd859c
Alopex6414 <1029535012@qq.com>
2019-08-15 21:37:23 +0800

    Rename 'nets_tcp.go'->'nets_tcp_server.go'.

---
+++ nets/nets_tcp_client.go
@@ -0,0 +1,19 @@
+package nets
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func StartTcpClient(ip string, port string) {
+	// connect to tcp server
+	conn, err := net.Dial("tcp", ip + ":" + port)
+	if err != nil {
+		fmt.Println("Error connect to server:", err)
+		log.Println("Error connect to server:", err)
+		os.Exit(1)
+	}
+	
+}
