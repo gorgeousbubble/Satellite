f97b29a6bdc62be66b6fa70896126d23f1978866
AuroraShine <hyx1029535012@icloud.com>
2019-08-16 17:01:02 +0800

    Change nets package content.

--- nets/nets_tcp_server.go
+++ nets/nets_tcp_server.go
@@ -10,14 +10,14 @@ import (
 func StartTcpServer(ip string, port string) {
 	// resolve ip address
 	fmt.Println("Start Tcp Server")
-	addr, err := net.ResolveTCPAddr("tcp", ip + ":" + port)
+	addr, err := net.ResolveTCPAddr("tcp", ip+":"+port)
 	if err != nil {
 		fmt.Println("Error resolve ip address:", err)
 		log.Println("Error resolve ip address:", err)
 		os.Exit(1)
 	}
 	// start listen tcp
-	fmt.Println("Listen Tcp:", ip + ":" + port)
+	fmt.Println("Listen Tcp:", ip+":"+port)
 	socket, err := net.ListenTCP("tcp", addr)
 	if err != nil {
 		fmt.Println("Error listen tcp:", err)
@@ -34,6 +34,6 @@ func StartTcpServer(ip string, port string) {
 			continue
 		}
 		fmt.Println("Success accept client:", conn.RemoteAddr().String())
-		go connRecvHandler(conn)
+		go connTcpRecvHandler(conn)
 	}
 }
