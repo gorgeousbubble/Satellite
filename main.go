3c7a7c7547e7a1153f9f7b671ec4a1f8ce157d51
Alopex6414 <1029535012@qq.com>
2019-08-18 11:34:37 +0800

    Start Multi-CPU support.

--- main.go
+++ main.go
@@ -4,17 +4,26 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
 	"satellite/cmd"
 	. "satellite/global"
 	_ "satellite/logging"
 )
 
+func init() {
+	// start multi-cpu
+	core := runtime.NumCPU()
+	runtime.GOMAXPROCS(core)
+}
+
 func main() {
+	// check command args number
 	if len(os.Args) < 2 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
+	// switch command execute
 	switch os.Args[1] {
 	case CmdPacket:
 		cmd.ParseCmdPack()
