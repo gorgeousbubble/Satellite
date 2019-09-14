f3fcd4c64d14ac2bda92ec149da977d554ec0b13
Alopex6414 <1029535012@qq.com>
2019-09-14 14:37:38 +0800

    Add test code.

--- unpack/unpack_test.go
+++ unpack/unpack_test.go
@@ -47,6 +47,56 @@ func TestUnpack5(t *testing.T) {
 	}
 }
 
+func TestWorkCalculate(t *testing.T) {
+	var work int64
+	var algorithm string
+	src := "../test/data/unpack/file_aes.txt"
+	err := WorkCalculate(src, &algorithm, &work)
+	if err != nil {
+		t.Fatal("Error Work Calculate:", err)
+	}
+}
+
+func TestWorkCalculate2(t *testing.T) {
+	var work int64
+	var algorithm string
+	src := "../test/data/unpack/file_des.txt"
+	err := WorkCalculate(src, &algorithm, &work)
+	if err != nil {
+		t.Fatal("Error Work Calculate:", err)
+	}
+}
+
+func TestWorkCalculate3(t *testing.T) {
+	var work int64
+	var algorithm string
+	src := "../test/data/unpack/file_3des.txt"
+	err := WorkCalculate(src, &algorithm, &work)
+	if err != nil {
+		t.Fatal("Error Work Calculate:", err)
+	}
+}
+
+func TestWorkCalculate4(t *testing.T) {
+	var work int64
+	var algorithm string
+	src := "../test/data/unpack/file_rsa.txt"
+	err := WorkCalculate(src, &algorithm, &work)
+	if err != nil {
+		t.Fatal("Error Work Calculate:", err)
+	}
+}
+
+func TestWorkCalculate5(t *testing.T) {
+	var work int64
+	var algorithm string
+	src := "../test/data/unpack/file_base64.txt"
+	err := WorkCalculate(src, &algorithm, &work)
+	if err != nil {
+		t.Fatal("Error Work Calculate:", err)
+	}
+}
+
 func BenchmarkUnpack(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := "../test/data/unpack/file_aes.txt"
@@ -98,6 +148,66 @@ func BenchmarkUnpack5(b *testing.B) {
 		err := Unpack(src, dest)
 		if err != nil {
 			b.Fatal("Error Unpack:", err)
+		}
+	}
+}
+
+func BenchmarkWorkCalculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var work int64
+		var algorithm string
+		src := "../test/data/unpack/file_aes.txt"
+		err := WorkCalculate(src, &algorithm, &work)
+		if err != nil {
+			b.Fatal("Error Work Calculate:", err)
+		}
+	}
+}
+
+func BenchmarkWorkCalculate2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var work int64
+		var algorithm string
+		src := "../test/data/unpack/file_des.txt"
+		err := WorkCalculate(src, &algorithm, &work)
+		if err != nil {
+			b.Fatal("Error Work Calculate:", err)
+		}
+	}
+}
+
+func BenchmarkWorkCalculate3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var work int64
+		var algorithm string
+		src := "../test/data/unpack/file_3des.txt"
+		err := WorkCalculate(src, &algorithm, &work)
+		if err != nil {
+			b.Fatal("Error Work Calculate:", err)
+		}
+	}
+}
+
+func BenchmarkWorkCalculate4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var work int64
+		var algorithm string
+		src := "../test/data/unpack/file_rsa.txt"
+		err := WorkCalculate(src, &algorithm, &work)
+		if err != nil {
+			b.Fatal("Error Work Calculate:", err)
+		}
+	}
+}
+
+func BenchmarkWorkCalculate5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var work int64
+		var algorithm string
+		src := "../test/data/unpack/file_base64.txt"
+		err := WorkCalculate(src, &algorithm, &work)
+		if err != nil {
+			b.Fatal("Error Work Calculate:", err)
 		}
 	}
 }
