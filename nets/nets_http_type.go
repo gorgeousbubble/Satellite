9a551ce86ed11de6bbf04bba46548b5cb48e3cf1
AuroraShine <hyx1029535012@icloud.com>
2019-09-29 11:21:28 +0800

    Add nets http unpack verbose methods.

--- nets/nets_http_type.go
+++ nets/nets_http_type.go
@@ -10,3 +10,17 @@ type TNetsUnpack struct {
 	Src  string `json:"src"`
 	Dest string `json:"dest"`
 }
+
+type TNetsUnpackVerboseReq struct {
+	Src string `json:"src"`
+}
+
+type TNetsUnpackVerboseResp struct {
+	files []TNetUnpackFileInfo `json:"files"`
+}
+
+type TNetUnpackFileInfo struct {
+	name      string `json:"name"`
+	size      string `json:"size"`
+	algorithm string `json:"type"`
+}
