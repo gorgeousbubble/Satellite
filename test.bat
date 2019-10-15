1394e8bc76442cfac96c96360436046e310188f7
AuroraShine <hyx1029535012@icloud.com>
2019-10-15 16:21:22 +0800

    Add test benchmark memory data.

--- test.bat
+++ test.bat
@@ -9,7 +9,7 @@ goto test_all
 :: Traversing folder test Golang code
 :test_all
 echo fmt all:
-for /d %%i in (*) do echo %%i & cd %%i & echo. & if not %%i == .github ( if not %%i == .idea ( if not %%i == .vscode ( if not %%i == 3pp ( if not %%i == bin ( if not %%i == doc ( if not %%i == log ( if not %%i == plugin ( if not %%i == test ( go test -v -cover -bench . ) ) ) ) ) ) ) ) ) & echo. & echo. & cd ..
+for /d %%i in (*) do echo %%i & cd %%i & echo. & if not %%i == .github ( if not %%i == .idea ( if not %%i == .vscode ( if not %%i == 3pp ( if not %%i == bin ( if not %%i == doc ( if not %%i == log ( if not %%i == plugin ( if not %%i == test ( go test -v -cover -benchmem -bench . ) ) ) ) ) ) ) ) ) & echo. & echo. & cd ..
 
 :: Pause format
 pause
