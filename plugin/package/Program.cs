d2239a09f4e220cb543b4d82a04f15c9b5b61986
Alopex6414 <1029535012@qq.com>
2019-10-20 11:41:56 +0800

    Change 'Form1' to 'main' & Add 2 pictures.

--- plugin/package/Program.cs
+++ plugin/package/Program.cs
@@ -16,7 +16,7 @@ namespace package
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new main());
         }
     }
 }
--- plugin/package/main.Designer.cs
+++ plugin/package/main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace package
 {
-    partial class Form1
+    partial class main
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "main";
+            this.Text = "package";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
