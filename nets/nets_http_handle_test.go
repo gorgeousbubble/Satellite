f898bd6144333c883064a045c1477b14ad39a737
Alopex6414 <1029535012@qq.com>
2019-10-26 12:43:51 +0800

    Add nets http pack process test code.

--- nets/nets_http_handle_test.go
+++ nets/nets_http_handle_test.go
@@ -68,6 +68,36 @@ func BenchmarkHandlePostNetsUnpack(b *testing.B) {
 	}
 }
 
+func TestHandleGetNetsPackProcess(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc(HttpURLPackProcess, handleNetsPackProcess)
+
+	writer := httptest.NewRecorder()
+	body := strings.NewReader(`{"src": ["../test/data/pack/file_1.txt", "../test/data/pack/file_2.txt", "../test/data/pack/file_3.txt", "../test/data/pack/file_4.txt", "../test/data/pack/file_5.txt"], "type": "aes"}`)
+	request, _ := http.NewRequest("GET", HttpURLPackProcess, body)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != http.StatusOK {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+}
+
+func BenchmarkHandleGetNetsPackProcess(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mux := http.NewServeMux()
+		mux.HandleFunc(HttpURLPackProcess, handleNetsPackProcess)
+
+		writer := httptest.NewRecorder()
+		body := strings.NewReader(`{"src": ["../test/data/pack/file_1.txt", "../test/data/pack/file_2.txt", "../test/data/pack/file_3.txt", "../test/data/pack/file_4.txt", "../test/data/pack/file_5.txt"], "type": "aes"}`)
+		request, _ := http.NewRequest("GET", HttpURLPackProcess, body)
+		mux.ServeHTTP(writer, request)
+
+		if writer.Code != http.StatusOK {
+			b.Errorf("Response code is %v", writer.Code)
+		}
+	}
+}
+
 func TestHandleGetNetsUnpackVerbose(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc(HttpURLUnpackVerbose, handleNetsUnpackVerbose)
