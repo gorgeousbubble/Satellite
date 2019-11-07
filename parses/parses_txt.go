dc38cf2acbbf3bf270f984e57c9be266c8c8c8de
AuroraShine <hyx1029535012@icloud.com>
2019-11-07 15:47:13 +0800

    Add parses txt files code(not finished)

---
+++ parses/parses_txt.go
@@ -0,0 +1,21 @@
+package parses
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+func parseCmdFiles(files []string) []string {
+	if runtime.GOOS == "windows" {
+		args := make([]string, 0, len(files))
+		for _, name := range files {
+			if matches, err := filepath.Glob(name); err != nil {
+				args = append(args, name)
+			} else if matches != nil {
+				args = append(args, matches...)
+			}
+		}
+		return args
+	}
+	return files
+}
