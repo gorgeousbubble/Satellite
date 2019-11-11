1acb2251bb9ecb3f42e3ced772d077e04da95235
AuroraShine <hyx1029535012@icloud.com>
2019-11-11 10:02:52 +0800

    Add parses txt test code.

--- parses/parses_txt_test.go
+++ parses/parses_txt_test.go
@@ -2,9 +2,16 @@ package parses
 
 import "testing"
 
-func TestParseTxtSimple(t *testing.T) {
+func TestParseTxtSimpleByWords(t *testing.T) {
 	file := "../test/data/parses/Alice’s Adventures in Wonderland.txt"
 	freq := map[string]int{}
 	upateWordsFreq(file, freq)
 	reportByWords(freq)
+}
+
+func TestParseTxtSimpleByFreq(t *testing.T) {
+	file := "../test/data/parses/Alice’s Adventures in Wonderland.txt"
+	freq := map[string]int{}
+	upateWordsFreq(file, freq)
+	reportByFreq(invertMap(freq))
 }
