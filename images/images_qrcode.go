d3bee7acd4ea3c9276df33b56a56128f4e844bfe
AuroraShine <hyx1029535012@icloud.com>
2019-11-11 16:32:50 +0800

    Add images generate to memory code.

--- images/images_qrcode.go
+++ images/images_qrcode.go
@@ -6,10 +6,20 @@ import (
 	"github.com/skip2/go-qrcode"
 )
 
-func QRCodeGenerate(content string, level qrcode.RecoveryLevel, size int, filename string) (err error) {
+func QRCodeGenerateToFile(content string, level qrcode.RecoveryLevel, size int, filename string) (err error) {
 	err = qrcode.WriteFile(content, level, size, filename)
 	if err != nil {
 		log.Println("Error generate qr code:", err)
+		return err
 	}
 	return err
+}
+
+func QRCodeGenerateToMemory(content string, level qrcode.RecoveryLevel, size int) (qr []byte, err error) {
+	qr, err = qrcode.Encode(content, level, size)
+	if err != nil {
+		log.Println("Error generate qr code to memory", err)
+		return qr, err
+	}
+	return qr, err
 }
--- images/images_qrcode_test.go
+++ images/images_qrcode_test.go
@@ -6,18 +6,34 @@ import (
 	"github.com/skip2/go-qrcode"
 )
 
-func TestQRCodeGenerate(t *testing.T) {
-	err := QRCodeGenerate("https://localhost:8080/", qrcode.Highest, 256, "../test/data/images/qrcode/qr_satellite.png")
+func TestQRCodeGenerateToFile(t *testing.T) {
+	err := QRCodeGenerateToFile("https://localhost:8080/", qrcode.Highest, 256, "../test/data/images/qrcode/qr_satellite.png")
 	if err != nil {
-		t.Errorf("Error generate qrcode: %v", err)
+		t.Errorf("Error generate qrcode to file: %v", err)
 	}
 }
 
-func BenchmarkQRCodeGenerate(b *testing.B) {
+func BenchmarkQRCodeGenerateToFile(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		err := QRCodeGenerate("https://localhost:8080/", qrcode.Highest, 256, "../test/data/images/qrcode/qr_satellite.png")
+		err := QRCodeGenerateToFile("https://localhost:8080/", qrcode.Highest, 256, "../test/data/images/qrcode/qr_satellite.png")
 		if err != nil {
-			b.Errorf("Error generate qrcode: %v", err)
+			b.Errorf("Error generate qrcode to file: %v", err)
+		}
+	}
+}
+
+func TestQRCodeGenerateToMemory(t *testing.T) {
+	_, err := QRCodeGenerateToMemory("https://localhost:8080/", qrcode.Highest, 256)
+	if err != nil {
+		t.Errorf("Error generate qrcode to memory: %v", err)
+	}
+}
+
+func BenchmarkQRCodeGenerateToMemory(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := QRCodeGenerateToMemory("https://localhost:8080/", qrcode.Highest, 256)
+		if err != nil {
+			b.Errorf("Error generate qrcode to memory: %v", err)
 		}
 	}
 }
