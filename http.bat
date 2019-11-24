7cafcdfd8bb16601cfd792777fbb5bcdfe432e8f
Alopex6414 <1029535012@qq.com>
2019-11-24 23:00:12 +0800

    Add http/https service start batch scripts.

---
+++ http.bat
@@ -0,0 +1,8 @@
+:: Satellite Project - HTTP Service
+:: Copyright(C) 2019, Team Gorgeous Bubble, All Rights Reserved.
+@echo off
+
+:: Satellite HTTP Service - Start
+cd bin
+satellite
+satellite http -ip localhost -port 8080
---
+++ https.bat
@@ -0,0 +1,8 @@
+:: Satellite Project - HTTPS Service
+:: Copyright(C) 2019, Team Gorgeous Bubble, All Rights Reserved.
+@echo off
+
+:: Satellite HTTPS Service - Start
+cd bin
+satellite
+satellite https -ip 127.0.0.1 -port 8080
