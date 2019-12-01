20cacc362944b1581195a720e31f9e78b0b5b148
Alopex6414 <1029535012@qq.com>
2019-12-01 17:11:08 +0800

    [feature]Add shell upx code.

--- shell/shell.go
+++ shell/shell.go
@@ -1,1 +1,16 @@
 package shell
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Shell(src string, dest string, algorithm string) (err error) {
+	switch algorithm {
+	case "upx":
+		err = shellUpx(src, dest)
+	default:
+		s := fmt.Sprint("Undefined shell algorithm.")
+		err = errors.New(s)
+	}
+}
--- shell/shell_upx.go
+++ shell/shell_upx.go
@@ -1,1 +1,55 @@
 package shell
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"satellite/exec"
+	"satellite/utils"
+)
+
+func shellUpx(src string, dest string) (err error) {
+	var b bool
+	// check source file exist...
+	b, err = utils.PathExist(src)
+	if err != nil {
+		log.Println("Error src file path:", err)
+		return err
+	}
+	if !b {
+		log.Println("Error src file path:Not exist")
+		return err
+	}
+	// according different system
+	if runtime.GOOS == "windows" {
+		// get executable dir
+		exe, err := os.Executable()
+		if err != nil {
+			log.Println("Error get executable path:", err)
+			return err
+		}
+		dir := filepath.Dir(exe)
+		// get upx path
+		cmd := dir + "\\tools\upx.exe -9 -o " + dest + " " + src
+		log.Println("Exec command:", cmd)
+		// exec command
+		r, err := exec.ExecCmd(cmd)
+		if err != nil {
+			log.Println("Error exec command:", err)
+			return err
+		}
+		log.Println("Exec result:", r)
+	} else {
+		// exec command
+		cmd := "../tools/upx -9 -o " + dest + " " + src
+		log.Println("Exec command:", cmd)
+		r, err := exec.ExecShell(cmd)
+		if err != nil {
+			log.Println("Error exec command:", err)
+			return err
+		}
+		log.Println("Exec result:", r)
+	}
+	return err
+}
