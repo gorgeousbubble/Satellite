931b46b80a7e85c88913662a82e0971410e6e67b
Alopex6414 <1029535012@qq.com>
2019-12-14 21:32:50 +0800

    [feature]Append parses ini command set value.

--- cmd/cmd_parses.go
+++ cmd/cmd_parses.go
@@ -8,21 +8,26 @@ import (
 	"os"
 	. "satellite/global"
 	"satellite/parses"
+	"strconv"
 )
 
 var parsesCmd = flag.NewFlagSet(CmdParses, flag.ExitOnError)
 var parsesResolver string
 var parsesSrc string
+var parsesMode string
 var parsesSection string
 var parsesName string
 var parsesType string
+var parsesValue string
 
 func init() {
 	parsesCmd.StringVar(&parsesResolver, "r", "", "parses package resolver, you can choose one from [\"ini\",\"erl\"]")
 	parsesCmd.StringVar(&parsesSrc, "i", "", "input file: correspond file need to be parse, such as \"test.ini\" or \"test.erl\"")
+	parsesCmd.StringVar(&parsesMode, "m", "", "parses mode: choose one of [\"get\",\"set\"]")
 	parsesCmd.StringVar(&parsesSection, "s", "", "parameter section: correspond parameter section only for ini, such as \"INIT\"")
 	parsesCmd.StringVar(&parsesName, "n", "", "parameter name: correspond parameter need to be parse, such as \"conf\" or \"pi\"")
 	parsesCmd.StringVar(&parsesType, "t", "", "parameter type: correspond parameter type need to be parse, such as \"bool\" or \"int\"")
+	parsesCmd.StringVar(&parsesValue, "v", "", "parameter value: correspond parameter only for set mode")
 }
 
 func ParseCmdParses() {
@@ -40,7 +45,7 @@ func ParseCmdParses() {
 	// handle command parameters
 	switch parsesResolver {
 	case "INI", "ini":
-		err = handleCmdParsesIni(parsesSrc, parsesSection, parsesName, parsesType)
+		err = handleCmdParsesIni(parsesSrc, parsesMode, parsesSection, parsesName, parsesType, parsesValue)
 	case "ERL", "erl":
 	default:
 		err = errors.New("unrecognized resolver")
@@ -52,39 +57,86 @@ func ParseCmdParses() {
 	fmt.Println("Parses success.")
 }
 
-func handleCmdParsesIni(src string, section string, name string, kind string) (err error) {
+func handleCmdParsesIni(src string, mode string, section string, name string, kind string, val string) (err error) {
 	// handle command parses ini
-	switch kind {
-	case "string":
-		var value string
-		err = parses.GetValueFrom(src, section, name, &value)
-		if err != nil {
-			return err
+	switch mode {
+	case "get":
+		switch kind {
+		case "string":
+			var value string
+			err = parses.GetValueFrom(src, section, name, &value)
+			if err != nil {
+				return err
+			}
+			fmt.Println("get value:", value)
+		case "int":
+			var value int
+			err = parses.GetValueFrom(src, section, name, &value)
+			if err != nil {
+				return err
+			}
+			fmt.Println("get value:", value)
+		case "float64":
+			var value float64
+			err = parses.GetValueFrom(src, section, name, &value)
+			if err != nil {
+				return err
+			}
+			fmt.Println("get value:", value)
+		case "bool":
+			var value bool
+			err = parses.GetValueFrom(src, section, name, &value)
+			if err != nil {
+				return err
+			}
+			fmt.Println("get value:", value)
+		default:
+			err = errors.New("unrecognized type name")
 		}
-		fmt.Println("get value:", value)
-	case "int":
-		var value int
-		err = parses.GetValueFrom(src, section, name, &value)
-		if err != nil {
-			return err
+	case "set":
+		switch kind {
+		case "string":
+			var value string
+			value = val
+			err = parses.SetValueTo(src, section, name, value)
+			if err != nil {
+				return err
+			}
+		case "int":
+			var value int
+			value, err = strconv.Atoi(val)
+			if err != nil {
+				return err
+			}
+			err = parses.SetValueTo(src, section, name, value)
+			if err != nil {
+				return err
+			}
+		case "float64":
+			var value float64
+			value, err = strconv.ParseFloat(val, 64)
+			if err != nil {
+				return err
+			}
+			err = parses.SetValueTo(src, section, name, value)
+			if err != nil {
+				return err
+			}
+		case "bool":
+			var value bool
+			value, err = strconv.ParseBool(val)
+			if err != nil {
+				return err
+			}
+			err = parses.SetValueTo(src, section, name, value)
+			if err != nil {
+				return err
+			}
+		default:
+			err = errors.New("unrecognized type name")
 		}
-		fmt.Println("get value:", value)
-	case "float64":
-		var value float64
-		err = parses.GetValueFrom(src, section, name, &value)
-		if err != nil {
-			return err
-		}
-		fmt.Println("get value:", value)
-	case "bool":
-		var value bool
-		err = parses.GetValueFrom(src, section, name, &value)
-		if err != nil {
-			return err
-		}
-		fmt.Println("get value:", value)
 	default:
-		err = errors.New("unrecognized type name")
+		err = errors.New("unrecognized parses mode")
 	}
 	return err
 }
