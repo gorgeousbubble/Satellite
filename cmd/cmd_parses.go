44d1ad860fd5d953efbc68a9c052116c940e3ec6
Alopex6414 <1029535012@qq.com>
2019-12-14 20:38:36 +0800

    [feature]Append get value of parses ini command

--- cmd/cmd_parses.go
+++ cmd/cmd_parses.go
@@ -3,20 +3,24 @@ package cmd
 import (
 	"flag"
 	"fmt"
+	"github.com/pkg/errors"
 	"log"
 	"os"
 	. "satellite/global"
+	"satellite/parses"
 )
 
 var parsesCmd = flag.NewFlagSet(CmdParses, flag.ExitOnError)
 var parsesResolver string
 var parsesSrc string
+var parsesSection string
 var parsesName string
 var parsesType string
 
 func init() {
 	parsesCmd.StringVar(&parsesResolver, "r", "", "parses package resolver, you can choose one from [\"ini\",\"erl\"]")
 	parsesCmd.StringVar(&parsesSrc, "i", "", "input file: correspond file need to be parse, such as \"test.ini\" or \"test.erl\"")
+	parsesCmd.StringVar(&parsesSection, "s", "", "parameter section: correspond parameter section only for ini, such as \"INIT\"")
 	parsesCmd.StringVar(&parsesName, "n", "", "parameter name: correspond parameter need to be parse, such as \"conf\" or \"pi\"")
 	parsesCmd.StringVar(&parsesType, "t", "", "parameter type: correspond parameter type need to be parse, such as \"bool\" or \"int\"")
 }
@@ -34,7 +38,13 @@ func ParseCmdParses() {
 		os.Exit(1)
 	}
 	// handle command parameters
-	err = handleCmdParses(parsesResolver, parsesSrc, parsesName, parsesType)
+	switch parsesResolver {
+	case "INI", "ini":
+		err = handleCmdParsesIni(parsesSrc, parsesSection, parsesName, parsesType)
+	case "ERL", "erl":
+	default:
+		err = errors.New("unrecognized resolver")
+	}
 	if err != nil {
 		fmt.Println("Parses failure:", err)
 		os.Exit(1)
@@ -42,7 +52,44 @@ func ParseCmdParses() {
 	fmt.Println("Parses success.")
 }
 
-func handleCmdParses(resolver string, src string, name string, kind string) (err error) {
-	// handle command parses
+func handleCmdParsesIni(src string, section string, name string, kind string) (err error) {
+	// handle command parses ini
+	switch kind {
+	case "string":
+		var value string
+		err = parses.GetValueFrom(src, section, name, &value)
+		if err != nil {
+			return err
+		}
+		fmt.Println("get value:", value)
+	case "int":
+		var value int
+		err = parses.GetValueFrom(src, section, name, &value)
+		if err != nil {
+			return err
+		}
+		fmt.Println("get value:", value)
+	case "float64":
+		var value float64
+		err = parses.GetValueFrom(src, section, name, &value)
+		if err != nil {
+			return err
+		}
+		fmt.Println("get value:", value)
+	case "bool":
+		var value bool
+		err = parses.GetValueFrom(src, section, name, &value)
+		if err != nil {
+			return err
+		}
+		fmt.Println("get value:", value)
+	default:
+		err = errors.New("unrecognized type name")
+	}
+	return err
+}
+
+func handleCmdParsesErl(src string, name string, kind string) (err error) {
+	// handle command parses erl
 	return err
 }
