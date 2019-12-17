9fc9a9943887d6dfc07a5e16a16ad40cdcce6cb9
AuroraShine <hyx1029535012@icloud.com>
2019-12-17 09:28:20 +0800

    [test]Append unpack confine test code.

--- unpack/unpack_test.go
+++ unpack/unpack_test.go
@@ -47,6 +47,15 @@ func TestUnpack5(t *testing.T) {
 	}
 }
 
+func TestUnpackConfine(t *testing.T) {
+	src := "../test/data/unpack/file_aes.txt"
+	dest := "../test/data/unpack/"
+	err := UnpackConfine(src, dest)
+	if err != nil {
+		t.Fatal("Error Unpack:", err)
+	}
+}
+
 func TestUnpackToFile(t *testing.T) {
 	src := "../test/data/unpack/file_aes.txt"
 	dest := "../test/data/unpack/"
