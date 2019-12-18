22dd4ef8d5ca7703fecf50c7e5c603145d4486eb
AuroraShine <hyx1029535012@icloud.com>
2019-12-18 09:28:11 +0800

    [test]Append unpack test code.

--- unpack/unpack_test.go
+++ unpack/unpack_test.go
@@ -389,6 +389,17 @@ func BenchmarkUnpackConfine2(b *testing.B) {
 	}
 }
 
+func BenchmarkUnpackConfine3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/unpack/file_3des.txt"
+		dest := "../test/data/unpack/"
+		err := UnpackConfine(src, dest)
+		if err != nil {
+			b.Fatal("Error Unpack:", err)
+		}
+	}
+}
+
 func BenchmarkUnpackToFile(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := "../test/data/unpack/file_aes.txt"
