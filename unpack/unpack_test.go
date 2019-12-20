9d78a2cba3cc9452bd9e35982a2432e1749b51eb
AuroraShine <hyx1029535012@icloud.com>
2019-12-20 10:05:37 +0800

    [test]Append unpack benchmark test code.

--- unpack/unpack_test.go
+++ unpack/unpack_test.go
@@ -580,6 +580,18 @@ func BenchmarkUnpackToFileConfine4(b *testing.B) {
 	}
 }
 
+func BenchmarkUnpackToFileConfine5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/unpack/file_base64.txt"
+		dest := "../test/data/unpack/"
+		target := "file_1.txt"
+		err := UnpackToFileConfine(src, target, dest)
+		if err != nil {
+			b.Fatal("Error Unpack To File:", err)
+		}
+	}
+}
+
 func BenchmarkUnpackToMemory(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		var dest []byte
