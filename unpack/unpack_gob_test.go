5b16b64d9839525e29275046b5c34ec46b77b1eb
AuroraShine <hyx1029535012@icloud.com>
2019-12-24 09:29:35 +0800

    [test]Append unpack gob test code.

---
+++ unpack/unpack_gob_test.go
@@ -0,0 +1,18 @@
+package unpack
+
+import (
+	"testing"
+)
+
+func TestGobDecodeFrom(t *testing.T) {
+	type test struct {
+		Name  string
+		Value int
+	}
+	src := "../test/data/pack/file_gob.gob"
+	out := test{}
+	err := GobDecodeFrom(src, &out)
+	if err != nil {
+		t.Fatal("Error gob decode from file:", err)
+	}
+}
