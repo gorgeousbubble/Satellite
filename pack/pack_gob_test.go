527b9240c1a0ced235bcb0b4cba8f60a744c1129
AuroraShine <hyx1029535012@icloud.com>
2019-12-26 09:46:57 +0800

    [test]Append pack gob test code.

--- pack/pack_gob_test.go
+++ pack/pack_gob_test.go
@@ -14,3 +14,18 @@ func TestGobEncodeTo(t *testing.T) {
 		t.Fatal("Error gob encode to file:", err)
 	}
 }
+
+func BenchmarkGobEncodeTo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		type test struct {
+			Name  string
+			Value int
+		}
+		src := "../test/data/pack/file_gob.gob"
+		in := test{Name: "test_gob", Value: 5}
+		err := GobEncodeTo(src, in)
+		if err != nil {
+			b.Fatal("Error gob encode to file:", err)
+		}
+	}
+}
