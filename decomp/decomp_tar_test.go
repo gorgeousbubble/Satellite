d79a271c8e1f45281b6e768158ac16cd7bc53171
Alopex6414 <1029535012@qq.com>
2019-12-28 00:14:35 +0800

    [test]Append test code.

--- decomp/decomp_tar_test.go
+++ decomp/decomp_tar_test.go
@@ -11,6 +11,17 @@ func TestDeCompressTar(t *testing.T) {
 	}
 }
 
+func BenchmarkDeCompressTar(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "../test/data/decomp/file.tar"
+		dest := "../test/data/decomp/"
+		err := DeCompressTar(src, dest)
+		if err != nil {
+			b.Fatal("Error DeCompress Tar:", err)
+		}
+	}
+}
+
 func TestDeCompressTarGz(t *testing.T) {
 	src := "../test/data/decomp/file.tar.gz"
 	dest := "../test/data/decomp/"
