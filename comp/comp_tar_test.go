71508fbe18cd652408d6dfa631de1a3429c15ae4
Alopex6414 <1029535012@qq.com>
2019-12-28 00:01:17 +0800

    [feature]Addition compress '.tar' code.

--- comp/comp_tar_test.go
+++ comp/comp_tar_test.go
@@ -2,6 +2,15 @@ package comp
 
 import "testing"
 
+func TestCompressTar(t *testing.T) {
+	src := []string{"../test/data/comp/file_1.txt", "../test/data/comp/file_2.txt", "../test/data/comp/file_3.txt", "../test/data/comp/file_4.txt", "../test/data/comp/file_5.txt"}
+	dest := "../test/data/comp/file.tar"
+	err := CompressTar(src, dest)
+	if err != nil {
+		t.Fatal("Error Compress Tar:", err)
+	}
+}
+
 func TestCompressTarGz(t *testing.T) {
 	src := []string{"../test/data/comp/file_1.txt", "../test/data/comp/file_2.txt", "../test/data/comp/file_3.txt", "../test/data/comp/file_4.txt", "../test/data/comp/file_5.txt"}
 	dest := "../test/data/comp/file.tar.gz"
