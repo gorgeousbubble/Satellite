c67da2d59e24b6e6d7057c34699a8637bc7048d7
Alopex6414 <1029535012@qq.com>
2019-12-28 00:03:34 +0800

    [test]Append compress test code.

--- comp/comp_tar_test.go
+++ comp/comp_tar_test.go
@@ -11,6 +11,17 @@ func TestCompressTar(t *testing.T) {
 	}
 }
 
+func BenchmarkCompressTar(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []string{"../test/data/comp/file_1.txt", "../test/data/comp/file_2.txt", "../test/data/comp/file_3.txt", "../test/data/comp/file_4.txt", "../test/data/comp/file_5.txt"}
+		dest := "../test/data/comp/file.tar"
+		err := CompressTar(src, dest)
+		if err != nil {
+			b.Fatal("Error Compress Tar:", err)
+		}
+	}
+}
+
 func TestCompressTarGz(t *testing.T) {
 	src := []string{"../test/data/comp/file_1.txt", "../test/data/comp/file_2.txt", "../test/data/comp/file_3.txt", "../test/data/comp/file_4.txt", "../test/data/comp/file_5.txt"}
 	dest := "../test/data/comp/file.tar.gz"
