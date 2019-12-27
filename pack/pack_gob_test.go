fc74238299e46a16bd4ed28d504f31e66e19b675
Alopex6414 <1029535012@qq.com>
2019-12-27 23:28:26 +0800

    [test]Append pack gob test code.

--- pack/pack_gob_test.go
+++ pack/pack_gob_test.go
@@ -1,6 +1,39 @@
 package pack
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestGobEncode(t *testing.T) {
+	type test struct {
+		Name  string
+		Value int
+	}
+	buf := bytes.Buffer{}
+	in := test{Name: "test_gob", Value: 5}
+	err := GobEncode(&buf, in)
+	if err != nil {
+		t.Fatal("Error gob encode:", err)
+	}
+	fmt.Println(buf.String())
+}
+
+func BenchmarkGobEncode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		type test struct {
+			Name  string
+			Value int
+		}
+		buf := bytes.Buffer{}
+		in := test{Name: "test_gob", Value: 5}
+		err := GobEncode(&buf, in)
+		if err != nil {
+			b.Fatal("Error gob encode:", err)
+		}
+	}
+}
 
 func TestGobEncodeTo(t *testing.T) {
 	type test struct {
