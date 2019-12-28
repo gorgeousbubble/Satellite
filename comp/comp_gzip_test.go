4bc93ae217c7fec5f82ec40fc466e6b67bfd25a4
Alopex6414 <1029535012@qq.com>
2019-12-28 10:49:55 +0800

    [test]Append test code.

---
+++ comp/comp_gzip_test.go
@@ -0,0 +1,23 @@
+package comp
+
+import "testing"
+
+func TestCompressGzip(t *testing.T) {
+	src := []string{"../test/data/comp/file_1.txt", "../test/data/comp/file_2.txt", "../test/data/comp/file_3.txt", "../test/data/comp/file_4.txt", "../test/data/comp/file_5.txt"}
+	dest := "../test/data/comp/file.gz"
+	err := CompressGzip(src, dest)
+	if err != nil {
+		t.Fatal("Error Compress gzip:", err)
+	}
+}
+
+func BenchmarkCompressGzip(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []string{"../test/data/comp/file_1.txt", "../test/data/comp/file_2.txt", "../test/data/comp/file_3.txt", "../test/data/comp/file_4.txt", "../test/data/comp/file_5.txt"}
+		dest := "../test/data/comp/file.gz"
+		err := CompressGzip(src, dest)
+		if err != nil {
+			b.Fatal("Error Compress gzip:", err)
+		}
+	}
+}
