a224d4b96374462bfc97365240200c47853dbca3
AuroraShine <hyx1029535012@icloud.com>
2020-01-04 12:31:16 +0800

    [feature]Append nets http code.

--- nets/nets_http_client.go
+++ nets/nets_http_client.go
@@ -1,2 +1,30 @@
 package nets
 
+import (
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func HttpGet(url string) (r []byte, err error) {
+	// http Get request...
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("Error http get request:", err)
+		return r, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		err = errors.New("response status code not ok")
+		log.Println("Error http response code:", err)
+		return r, err
+	}
+	// start read response body...
+	defer resp.Body.Close()
+	r, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error read response body:", err)
+		return r, err
+	}
+	return r, err
+}
