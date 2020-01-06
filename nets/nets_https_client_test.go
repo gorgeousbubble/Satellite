b3cc91fbd5ff31ecdbd5daccb4edd1bbd136ff53
AuroraShine <hyx1029535012@icloud.com>
2020-01-06 09:37:37 +0800

    [test]Append nets https client test code.

--- nets/nets_https_client_test.go
+++ nets/nets_https_client_test.go
@@ -2,6 +2,7 @@ package nets
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 )
 
@@ -21,5 +22,16 @@ func BenchmarkHttpsGet(b *testing.B) {
 		if err != nil {
 			b.Fatal("Error https get:", err)
 		}
+	}
+}
+
+func TestHttpsHead(t *testing.T) {
+	url := "https://github.com"
+	code, err := HttpsHead(url)
+	if err != nil {
+		t.Fatal("Error https head:", err)
+	}
+	if code != http.StatusOK {
+		t.Fatal("Error https head response status code")
 	}
 }
