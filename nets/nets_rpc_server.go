3b6459ca70563250d4e6df57d43f3f0fe03093bc
AuroraShine <hyx1029535012@icloud.com>
2020-01-11 14:44:02 +0800

    [feature]Add nets rpc tcp server function.

--- nets/nets_rpc_server.go
+++ nets/nets_rpc_server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/http"
 	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
 func StartRpcHttpServer(ip string, port string) (err error) {
@@ -30,5 +31,37 @@ func StartRpcHttpServer(ip string, port string) (err error) {
 		log.Println("Error serve http:", err)
 		return err
 	}
+	return err
+}
+
+func StartRpcTcpServer(ip string, port string) (err error) {
+	// rpc register interface...
+	err = rpc.Register(new(GoApi))
+	if err != nil {
+		log.Println("Error RPC register interface:", err)
+		return err
+	}
+	// rpc start tcp service...
+	l, err := net.Listen("tcp", ip+":"+port)
+	if err != nil {
+		log.Println("Error listen tcp:", err)
+		return err
+	}
+	fmt.Println("Start Listen And Server on ", ip+":"+port)
+	// rpc loop wait for connect...
+	go func(l net.Listener) {
+		for {
+			// client connect to server
+			conn, err := l.Accept()
+			if err != nil {
+				continue
+			}
+			// handle json transfer
+			go func(conn net.Conn) {
+				jsonrpc.ServeConn(conn)
+				fmt.Println("RPC client connect to server:", conn.RemoteAddr())
+			}(conn)
+		}
+	}(l)
 	return err
 }
