0c2fa0e27928274fe365d3b1bd4f0d4f66253e00
Alopex6414 <1029535012@qq.com>
2020-02-13 11:09:29 +0800

    [improve]Add function comments.

--- pack/pack_aes.go
+++ pack/pack_aes.go
@@ -19,6 +19,12 @@ import (
 	"time"
 )
 
+// PackAES function
+// input source file list and dest package path, output error information
+// src file support both absolute and relative paths, like 'C:\\file.txt' or '../test/data/file.txt'
+// dest file also support both absolute and relative paths, like 'C:\\package.pak' or '../test/data/package.pak'
+// dest file name suffix can be any type such as '.pak', '.dat', even none is ok
+// return err indicate the success or failure function execute
 func PackAES(src []string, dest string) (err error) {
 	wg := &sync.WaitGroup{}
 	// start multi-cpu
