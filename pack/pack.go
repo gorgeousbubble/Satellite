f875b6ec363f4223a0c793097252fa32598057f2
Alopex6414 <1029535012@qq.com>
2020-02-20 09:57:37 +0800

    [improve]Append comments.

--- pack/pack.go
+++ pack/pack.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 )
 
+// Pack function
+// input src file list, output dest file path and algorithm which used in pack, return error info
+// this function will base on algorithm to call correspond function
+// src file support both absolute and relative paths, like 'C:\\file.txt' or '../test/data/file.txt'
+// dest file also support both absolute and relative paths, like 'C:\\package.pak' or '../test/data/package.pak'
+// algorithm now support 'AES', 'DES', '3DES', 'RSA' and 'BASE64', you can send both up case and low case
+// return err indicate the success or failure function execute
 func Pack(src []string, dest string, algorithm string) (err error) {
 	switch algorithm {
 	case "AES", "aes":
