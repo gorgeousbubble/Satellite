e74d7e5a3d53d957f85b8aa5abf042c7dcdc2b55
Alopex6414 <1029535012@qq.com>
2020-02-27 09:58:34 +0800

    [improve]Append comments

--- pack/pack_sha256.go
+++ pack/pack_sha256.go
@@ -7,7 +7,7 @@ import (
 )
 
 // SHA256Check function
-// input src string and output dest string with sha1
+// input src string and output dest string with sha256
 // return bool indicate the success or failure function execute
 func SHA256Check(src string, dest string) bool {
 	b := false
@@ -17,17 +17,23 @@ func SHA256Check(src string, dest string) bool {
 	return b
 }
 
+// SHA256Encode function
+// encode src string output dest string with sha256
 func SHA256Encode(src string) string {
 	h := sha256.New()
 	h.Write([]byte(src))
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+// SHA256EncryptGo function
+// encrypt sha256 with goroutine
 func SHA256EncryptGo(data []byte, r *[sha256.Size]byte, wg *sync.WaitGroup) {
 	defer wg.Done()
 	*r = sha256.Sum256(data)
 }
 
+// SHA256Encrypt function
+// encrypt sha256
 func SHA256Encrypt(data []byte) [sha256.Size]byte {
 	return sha256.Sum256(data)
 }
