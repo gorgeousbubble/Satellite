157b3818ae991f6aee4ba3343b9ac9a1ecbd35eb
Alopex6414 <1029535012@qq.com>
2020-02-28 17:44:07 +0800

    [improve]Append commnets.

--- pack/pack_sha512.go
+++ pack/pack_sha512.go
@@ -17,17 +17,23 @@ func SHA512Check(src string, dest string) bool {
 	return b
 }
 
+// SHA512Encode function
+// encode src string output dest string with sha512
 func SHA512Encode(src string) string {
 	h := sha512.New()
 	h.Write([]byte(src))
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+// SHA512EncryptGo function
+// encrypt sha512 with goroutine
 func SHA512EncryptGo(data []byte, r *[sha512.Size]byte, wg *sync.WaitGroup) {
 	defer wg.Done()
 	*r = sha512.Sum512(data)
 }
 
+// SHA512Encrypt function
+// encrypt sha512
 func SHA512Encrypt(data []byte) [sha512.Size]byte {
 	return sha512.Sum512(data)
 }
