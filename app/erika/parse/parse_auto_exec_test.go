4c14716cac848bf0304e2cdb77f2f9b2608341c8
Alopex6414 <1029535012@qq.com>
2020-03-09 16:38:27 +0800

    Add test code

--- app/erika/parse/parse_auto_exec_test.go
+++ app/erika/parse/parse_auto_exec_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestUnmarshalAutoExecJson(t *testing.T) {
-	path := "../template/auto_exec.json"
+	path := "../test/data/parse/auto_exec.json"
 	style := "json"
 	out := TAutoExec{}
 	err := unmarshalAutoExec(path, &out, style)
@@ -16,12 +16,80 @@ func TestUnmarshalAutoExecJson(t *testing.T) {
 
 func BenchmarkUnmarshalAutoExecJson(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		path := "../template/auto_exec.json"
+		path := "../test/data/parse/auto_exec.json"
 		style := "json"
 		out := TAutoExec{}
 		err := unmarshalAutoExec(path, &out, style)
 		if err != nil {
 			b.Fatal("Error unmarshal auto exec:", err)
+		}
+	}
+}
+
+func TestMarshalAutoExecJson(t *testing.T) {
+	path := "../test/data/parse/auto_exec.json"
+	style := "json"
+	in := TAutoExec{
+		Name:        "auto_exec_sync_git",
+		Description: "Automatic synchronization Git with remote repository.",
+		Schedule:    "@hourly",
+		Attribute: TAutoExecAttribute{
+			Location: "/home/pi/repos/satellite",
+		},
+		Task: TAutoExecTask{
+			Exec: []TAutoExecTaskCommand{
+				{
+					Command: "git pull gogs master",
+				},
+				{
+					Command: "git add .",
+				},
+				{
+					Command: "git commit -m 'Automatic Commit by Erika.'",
+				},
+				{
+					Command: "git push gogs master",
+				},
+			},
+		},
+	}
+	err := marshalAutoExec(path, &in, style)
+	if err != nil {
+		t.Fatal("Error marshal auto exec:", err)
+	}
+}
+
+func BenchmarkMarshalAutoExecJson(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		path := "../test/data/parse/auto_exec.json"
+		style := "json"
+		in := TAutoExec{
+			Name:        "auto_exec_sync_git",
+			Description: "Automatic synchronization Git with remote repository.",
+			Schedule:    "@hourly",
+			Attribute: TAutoExecAttribute{
+				Location: "/home/pi/repos/satellite",
+			},
+			Task: TAutoExecTask{
+				Exec: []TAutoExecTaskCommand{
+					{
+						Command: "git pull gogs master",
+					},
+					{
+						Command: "git add .",
+					},
+					{
+						Command: "git commit -m 'Automatic Commit by Erika.'",
+					},
+					{
+						Command: "git push gogs master",
+					},
+				},
+			},
+		}
+		err := marshalAutoExec(path, &in, style)
+		if err != nil {
+			b.Fatal("Error marshal auto exec:", err)
 		}
 	}
 }
