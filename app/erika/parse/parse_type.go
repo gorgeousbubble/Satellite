00ab1ea979a0134c1f18abba6235b6a632c40506
Alopex6414 <1029535012@qq.com>
2020-03-09 14:37:46 +0800

    Add structure label yaml

--- app/erika/parse/parse_type.go
+++ app/erika/parse/parse_type.go
@@ -2,20 +2,20 @@ package parse
 
 // Auto execute task template structure...
 type TAutoExec struct {
-	Name        string             `json:"name"`
-	Description string             `json:"description"`
-	Schedule    string             `json:"schedule"`
-	Attribute   TAutoExecAttribute `json:"attribute"`
+	Name        string             `json:"name" yaml:"name"`
+	Description string             `json:"description" yaml:"description"`
+	Schedule    string             `json:"schedule" yaml:"schedule"`
+	Attribute   TAutoExecAttribute `json:"attribute" yaml:"attribute"`
 }
 
 type TAutoExecAttribute struct {
-	Location string `json:"location"`
+	Location string `json:"location" yaml:"location"`
 }
 
 type TAutoExecTask struct {
-	Exec []TAutoExecTaskCommand `json:"exec"`
+	Exec []TAutoExecTaskCommand `json:"exec" yaml:"exec"`
 }
 
 type TAutoExecTaskCommand struct {
-	Command string `json:"command"`
+	Command string `json:"command" yaml:"command"`
 }
