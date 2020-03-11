4bd0fb3f7b39bb8dc38774bc636df1799baa84d4
Alopex6414 <1029535012@qq.com>
2020-03-11 17:23:26 +0800

    fix issue that exec only once execution.

--- app/erika/proc/proc_auto_exec.go
+++ app/erika/proc/proc_auto_exec.go
@@ -8,22 +8,13 @@ import (
 func HandleAutoExec(location string, commands []string) (err error) {
 	var s string
 	var r string
-	// combine command and record it
-	s = "cd " + location
-	logs.Info("Execute command:", s)
-	// enter location which will execute command
-	r, err = exec.Exec(s)
-	if err != nil {
-		logs.Error("Error execute command:", err)
-		logs.Error("Result:", r)
-		return err
-	}
-	logs.Info("Result:", r)
 	// execute all commands...
 	for _, v := range commands {
-		logs.Info("Execute command:", v)
+		// combine command and record it
+		s = "cd " + location + ";" + v
+		logs.Info("Execute command:", s)
 		// execute one command
-		r, err = exec.Exec(v)
+		r, err = exec.Exec(s)
 		if err != nil {
 			logs.Error("Error execute command:", err)
 			logs.Error("Result:", r)
