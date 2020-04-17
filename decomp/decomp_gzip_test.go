da43edf44aebdfdf9397c52d8c050247ce8668b1
AuroraShine <hyx1029535012@icloud.com>
2020-04-17 13:56:07 +0800

    [improve]Append comments

--- decomp/decomp_gzip_test.go
+++ decomp/decomp_gzip_test.go
@@ -2,6 +2,7 @@ package decomp
 
 import "testing"
 
+// TestDeCompressGzip function
 func TestDeCompressGzip(t *testing.T) {
 	src := []string{"../test/data/decomp/file_1.gz", "../test/data/decomp/file_2.gz", "../test/data/decomp/file_3.gz", "../test/data/decomp/file_4.gz", "../test/data/decomp/file_5.gz"}
 	dest := "../test/data/decomp"
@@ -11,6 +12,7 @@ func TestDeCompressGzip(t *testing.T) {
 	}
 }
 
+// BenchmarkDeCompressGzip function
 func BenchmarkDeCompressGzip(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := []string{"../test/data/decomp/file_1.gz", "../test/data/decomp/file_2.gz", "../test/data/decomp/file_3.gz", "../test/data/decomp/file_4.gz", "../test/data/decomp/file_5.gz"}
