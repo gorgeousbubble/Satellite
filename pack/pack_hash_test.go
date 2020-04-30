9ede317dcc8bb552942344cc85e124814a311dca
AuroraShine <hyx1029535012@icloud.com>
2020-04-30 10:57:31 +0800

    [feature]Append test function code

--- pack/pack_hash_test.go
+++ pack/pack_hash_test.go
@@ -15,6 +15,17 @@ func TestPackHashEncodeMD5(t *testing.T) {
 	fmt.Println("Hash md5 encode:", dest)
 }
 
+func BenchmarkPackHashEncodeMD5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		algorithm := "md5"
+		_, err := PackHashEncode(src, algorithm)
+		if err != nil {
+			b.Fatal("Error pack hash encode:", err)
+		}
+	}
+}
+
 func TestPackHashEncodeSHA1(t *testing.T) {
 	src := "hello,world!"
 	algorithm := "sha1"
@@ -23,6 +34,17 @@ func TestPackHashEncodeSHA1(t *testing.T) {
 		t.Fatal("Error pack hash encode:", err)
 	}
 	fmt.Println("Hash sha1 encode:", dest)
+}
+
+func BenchmarkPackHashEncodeSHA1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		algorithm := "sha1"
+		_, err := PackHashEncode(src, algorithm)
+		if err != nil {
+			b.Fatal("Error pack hash encode:", err)
+		}
+	}
 }
 
 func TestPackHashEncodeSHA256(t *testing.T) {
@@ -35,6 +57,17 @@ func TestPackHashEncodeSHA256(t *testing.T) {
 	fmt.Println("Hash sha256 encode:", dest)
 }
 
+func BenchmarkPackHashEncodeSHA256(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		algorithm := "sha256"
+		_, err := PackHashEncode(src, algorithm)
+		if err != nil {
+			b.Fatal("Error pack hash encode:", err)
+		}
+	}
+}
+
 func TestPackHashEncodeSHA512(t *testing.T) {
 	src := "hello,world!"
 	algorithm := "sha512"
@@ -43,4 +76,15 @@ func TestPackHashEncodeSHA512(t *testing.T) {
 		t.Fatal("Error pack hash encode:", err)
 	}
 	fmt.Println("Hash sha512 encode:", dest)
+}
+
+func BenchmarkPackHashEncodeSHA512(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		algorithm := "sha512"
+		_, err := PackHashEncode(src, algorithm)
+		if err != nil {
+			b.Fatal("Error pack hash encode:", err)
+		}
+	}
 }
