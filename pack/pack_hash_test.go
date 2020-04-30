6f7f11e847452a3150fd6a4267ee96e833567203
AuroraShine <hyx1029535012@icloud.com>
2020-04-30 10:32:25 +0800

    [feature]Append test function code

--- pack/pack_hash_test.go
+++ pack/pack_hash_test.go
@@ -1,1 +1,17 @@
 package pack
+
+import (
+	"testing"
+)
+
+func TestPackHashEncodeMD5(t *testing.T) {
+	src := "hello,world!"
+	algorithm := "md5"
+	dest, err := PackHashEncode(src, algorithm)
+	if err != nil {
+		t.Fatal("Error pack hash encode:", err)
+	}
+	if dest != "c0e84e870874dd37ed0d164c7986f03a" {
+		t.Fatal("Error encode result")
+	}
+}
