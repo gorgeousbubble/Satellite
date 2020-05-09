0bc0812586ca58c31bb8cb427e4208b2b099f29d
AuroraShine <hyx1029535012@icloud.com>
2020-05-09 10:18:52 +0800

    [improve]Append comments

--- cmd/cmd_http.go
+++ cmd/cmd_http.go
@@ -17,6 +17,11 @@ func init() {
 	httpCmd.StringVar(&httpPort, "port", "14514", "port: port number witch http server listen, such as \"14514\"")
 }
 
+// ParseCmdHttp function
+// this function will be called in main.go and parse and execute one http command
+// ip address support both IPv4 and IPv6, you can send parameter like 'localhost', '192.168.1.100', etc.
+// port is the integer number between 1~65535, please use the reserve port range (50000~65535), such as '55555'
+// any failure or error will output print to screen and exit process
 func ParseCmdHttp() {
 	// check args number
 	if len(os.Args) == 2 {
@@ -33,6 +38,9 @@ func ParseCmdHttp() {
 	handleCmdHttp(httpIp, httpPort)
 }
 
+// function handleCmdHttp
+// this function mainly handle the main flow of command
+// any error will break and exit
 func handleCmdHttp(ip string, port string) {
 	nets.StartHttpServer(ip, port)
 }
