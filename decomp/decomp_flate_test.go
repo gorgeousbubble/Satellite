e26f76ff2c0a42421dca31b8d301bff3df6f9612
AuroraShine <hyx1029535012@icloud.com>
2020-05-13 10:19:39 +0800

    [feature]Append test code

--- decomp/decomp_flate_test.go
+++ decomp/decomp_flate_test.go
@@ -14,3 +14,13 @@ func TestDeCompressFlate(t *testing.T) {
 	fmt.Println("The src:", string(src))
 	fmt.Println("The dest:", string(dest))
 }
+
+func BenchmarkDeCompressFlate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := []byte{202, 72, 205, 201, 201, 215, 41, 207, 47, 202, 73, 81, 4, 0, 0, 0, 255, 255}
+		_, err := DeCompressFlate(src)
+		if err != nil {
+			b.Fatal("Error decompress flate:", err)
+		}
+	}
+}
