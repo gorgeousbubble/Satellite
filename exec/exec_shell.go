9743d6ce4ebe81afc528ddbd8fc1a6e777f8fdf3
AuroraShine <hyx1029535012@icloud.com>
2020-05-26 10:16:27 +0800

    [improve]Append comments

--- exec/exec_shell.go
+++ exec/exec_shell.go
@@ -5,6 +5,10 @@ import (
 	"os/exec"
 )
 
+// ExecShell function
+// this function is mainly used to execute command in Linux
+// input command string which will be executed in target os system
+// return execute result and err indicate the success or failure function execute
 func ExecShell(s string) (r string, err error) {
 	var out bytes.Buffer
 	cmd := exec.Command("/bin/bash", "-c", s)
