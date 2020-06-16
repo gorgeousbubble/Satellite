2bc6302fc6f4c556795245c659c03f1791352113
AuroraShine <hyx1029535012@icloud.com>
2020-06-16 10:03:40 +0800

    [feature]Append blake2b check function

--- pack/pack_blake2b_test.go
+++ pack/pack_blake2b_test.go
@@ -82,3 +82,12 @@ func TestBlake2bCheck256(t *testing.T) {
 		t.Fatal("Error check blake2b 256 encode:", r)
 	}
 }
+
+func TestBlake2bCheck512(t *testing.T) {
+	src := "hello,world!"
+	dest := "9fda5311802ea6b2e54fe5da18206584aed4a488cf8a06553e8b98f11f61d2b2115b9aba70c61d29bca31f444059cdba2f262a60358d23b0f661f75d5b91213f"
+	r := Blake2bCheck512(src, dest)
+	if !r {
+		t.Fatal("Error check blake2b 512 encode:", r)
+	}
+}
