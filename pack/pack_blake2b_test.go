1b8e3709cd81256f0c056c8cc96722f874632168
AuroraShine <hyx1029535012@icloud.com>
2020-06-17 11:07:30 +0800

    [feature]Add blake2b test code

--- pack/pack_blake2b_test.go
+++ pack/pack_blake2b_test.go
@@ -94,6 +94,17 @@ func TestBlake2bCheck256(t *testing.T) {
 	}
 }
 
+func BenchmarkBlake2bCheck256(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := "hello,world!"
+		dest := "8268578331a07de98347abd8cf11addf924a4ea0ac75e4aec1bf3fe6cb314553"
+		r := Blake2bCheck256(src, dest)
+		if !r {
+			b.Fatal("Error check blake2b 256 encode:", r)
+		}
+	}
+}
+
 func TestBlake2bCheck512(t *testing.T) {
 	src := "hello,world!"
 	dest := "9fda5311802ea6b2e54fe5da18206584aed4a488cf8a06553e8b98f11f61d2b2115b9aba70c61d29bca31f444059cdba2f262a60358d23b0f661f75d5b91213f"
