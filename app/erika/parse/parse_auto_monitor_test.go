65cadfcb0e86eec64cef60ad12881009587a239c
AuroraShine <hyx1029535012@icloud.com>
2020-06-18 10:52:33 +0800

    [feature]Append erika test code

--- app/erika/parse/parse_auto_monitor_test.go
+++ app/erika/parse/parse_auto_monitor_test.go
@@ -44,3 +44,26 @@ func TestMarshalAutoMonitorJson(t *testing.T) {
 		t.Fatal("Error marshal auto monitor:", err)
 	}
 }
+
+func BenchmarkMarshalAutoMonitorJson(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		path := "../test/data/parse/auto_exec.json"
+		style := "json"
+		in := TAutoMonitor{
+			Name:        "auto_rasp_monitor",
+			Description: "Automatic monitor room by raspberry camera.",
+			Schedule:    "@hourly",
+			Attribute: TAutoMonitorAttribute{
+				Location: "/home/pi/monitor",
+				Period: "600000",
+				Format: "h264",
+				Width: "1920",
+				Height: "1080",
+			},
+		}
+		err := marshalAutoMonitor(path, &in, style)
+		if err != nil {
+			b.Fatal("Error marshal auto monitor:", err)
+		}
+	}
+}
