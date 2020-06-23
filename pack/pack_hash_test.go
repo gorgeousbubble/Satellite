0cdc7df03fe2735cde7f48d6d5d746d57c4e8664
AuroraShine <hyx1029535012@icloud.com>
2020-06-23 10:08:12 +0800

    [feature]Append function test code

--- pack/pack_hash_test.go
+++ pack/pack_hash_test.go
@@ -151,3 +151,55 @@ func BenchmarkPackHashEncodeBlake2b512(b *testing.B) {
 		}
 	}
 }
+
+func TestPackHashCheckMD5(t *testing.T) {
+	src := "hello,world!"
+	dest := "c0e84e870874dd37ed0d164c7986f03a"
+	algorithm := "md5"
+	result, err := PackHashCheck(src, dest, algorithm)
+	if err != nil {
+		t.Fatal("Error pack hash check:", err)
+	}
+	if !result {
+		t.Fatal("Error pack hash check result")
+	}
+}
+
+func TestPackHashCheckSHA1(t *testing.T) {
+	src := "hello,world!"
+	dest := "4518135c05e0706c0a34168996517bb3f28d94b5"
+	algorithm := "sha1"
+	result, err := PackHashCheck(src, dest, algorithm)
+	if err != nil {
+		t.Fatal("Error pack hash check:", err)
+	}
+	if !result {
+		t.Fatal("Error pack hash check result")
+	}
+}
+
+func TestPackHashCheckSHA256(t *testing.T) {
+	src := "hello,world!"
+	dest := "ec1e0bd875226943ad0e8877bdba4ca449c4cb8591a5363921c9f1ee20084c34"
+	algorithm := "sha256"
+	result, err := PackHashCheck(src, dest, algorithm)
+	if err != nil {
+		t.Fatal("Error pack hash check:", err)
+	}
+	if !result {
+		t.Fatal("Error pack hash check result")
+	}
+}
+
+func TestPackHashCheckSHA512(t *testing.T) {
+	src := "hello,world!"
+	dest := "fa9edcfdaab7a4165f8d2593f04077d46aca957493e7e181ca479582d519a299d967305294d5d46fb5e0158240441b94cd96510c2311bdc86870e5ebf3efe60c"
+	algorithm := "sha256"
+	result, err := PackHashCheck(src, dest, algorithm)
+	if err != nil {
+		t.Fatal("Error pack hash check:", err)
+	}
+	if !result {
+		t.Fatal("Error pack hash check result")
+	}
+}
