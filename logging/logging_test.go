25edbc6a4c2319a64499e7a60545190dd9e3e7a2
AuroraShine <hyx1029535012@icloud.com>
2020-07-03 10:11:01 +0800

    [feature]Append logging code

--- logging/logging_test.go
+++ logging/logging_test.go
@@ -20,23 +20,36 @@ func TestLoggingWithParameters(t *testing.T) {
 	log.Printf("a + b = %v\n", a + b)
 }
 
+// TestLoggingWithCycle function
+// test logging package with cycle
 func TestLoggingWithCycle(t *testing.T) {
 	for i := 0; i < 10000; i++ {
 		log.Println("Count:", i)
 	}
 }
 
+// BenchmarkLoggingPlain function
 func BenchmarkLoggingPlain(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		log.Println("hello,world!")
 	}
 }
 
+// BenchmarkLoggingWithParameters function
 func BenchmarkLoggingWithParameters(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		a, b := 3, 5
 		log.Println("a =", a)
 		log.Println("b =", b)
 		log.Printf("a + b = %v\n", a + b)
+	}
+}
+
+// BenchmarkLoggingWithCycle function
+func BenchmarkLoggingWithCycle(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < 10000; i++ {
+			log.Println("hello,world!")
+		}
 	}
 }
