d7ab612af954ccfc4cad9899f1c22b69016702a8
AuroraShine <hyx1029535012@icloud.com>
2020-08-18 10:46:27 +0800

    [improve]Append logs package test code.

--- logs/logs_test.go
+++ logs/logs_test.go
@@ -23,6 +23,20 @@ func TestFileLog(t *testing.T) {
 	l.Debug("hello,world!")
 }
 
+func TestFileLogOutput(t *testing.T) {
+	w, err := NewFileWriter("./log/satellite.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND)
+	if err != nil {
+		t.Fatal("Error new file writer:", err)
+	}
+	l := NewDefaultLogger(w)
+	l.Debug("hello!")
+	l.Info("Can you speak English?")
+	l.Trace("Welcome!")
+	l.Warning("What a nice day!")
+	l.Error("Something error has happened!")
+	l.Cirtical("Critical Fatal...")
+}
+
 func TestMultipleFileLog(t *testing.T) {
 	w, err := NewMultipleFileWriter("./log/satellite.log", 1*1024*1024, 5)
 	if err != nil {
